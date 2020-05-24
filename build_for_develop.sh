869f23e17173ca06108147f73161a2d4858ed064
Diptorup Deb <diptorup.deb@intel.com>
2020-05-24 01:47:53 -0500

    Add a build_for_develop script for dppy

---
+++ build_for_develop.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+0;136;0c
+rm -rf build
+mkdir build
+cd build
+
+INSTALL_PREFIX=`pwd`/install
+DPCPP_ROOT=/opt/intel/inteloneapi/compiler/latest
+
+cmake                                                       \
+    -DCMAKE_BUILD_TYPE=Release                              \
+    -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX}                \
+    -DCMAKE_PREFIX_PATH=${INSTALL_PREFIX}                   \
+    -DCMAKE_C_COMPILER:PATH=${DPCPP_ROOT}/linux/bin/clang   \
+    -DCMAKE_CXX_COMPILER:PATH=${DPCPP_ROOT}/linux/bin/dpcpp \
+    ..
+
+make -n -j 4 && make install
+
+cd ../python_binding
+export DP_GLUE_LIBDIR=${INSTALL_PREFIX}/lib
+export DP_GLUE_INCLDIR=${INSTALL_PREFIX}/include
+export OPENCL_LIBDIR=/usr/lib/x86_64-linux-gnu/
+export DPPY_ONEAPI_INTERFACE_LIBDIR=${INSTALL_PREFIX}/lib
+export DPPY_ONEAPI_INTERFACE_INCLDIR=${INSTALL_PREFIX}/include
+
+export CC=clang
+export CXX=dpcpp
+# FIXME: How to pass this using setup.py? This flags is needed when
+# dpcpp compiles the generated cpp file.
+export CFLAGS=-fPIC
+python setup.py clean --all
+python setup.py build_ext --inplace
+python setup.py develop
