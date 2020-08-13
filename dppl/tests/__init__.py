b0b25d7bd6c0d4d007b8d6b7a987eeaf8bc94a90
Sergey Pokhodenko <sergey.pokhodenko@intel.com>
2020-08-14 00:47:11 +0300

    Enable tests (#17)

---
+++ dppl/tests/__init__.py
@@ -0,0 +1,1 @@
+from .test_dump_functions import *
--- dppl/tests/test_dump_functions.py
+++ dppl/tests/test_dump_functions.py
@@ -14,67 +14,16 @@
 # limitations under the License.
 #******************************************************************************/
 
-from contextlib import contextmanager
-import ctypes
-import dppl
-import io
-import os, sys
-import tempfile
 import unittest
 
+import dppl
 import dppl.ocldrv as drv
-
-
-libc = ctypes.CDLL(None)
-c_stdout = ctypes.c_void_p.in_dll(libc, 'stdout')
-
-# Sourced from
-# https://eli.thegreenplace.net/2015/redirecting-all-kinds-of-stdout-in-python/
-@contextmanager
-def stdout_redirector(stream):
-    # The original fd stdout points to. Usually 1 on POSIX systems.
-    original_stdout_fd = sys.stdout.fileno()
-
-    def _redirect_stdout(to_fd):
-        """Redirect stdout to the given file descriptor."""
-        # Flush the C-level buffer stdout
-        libc.fflush(c_stdout)
-        # Flush and close sys.stdout - also closes the file descriptor (fd)
-        sys.stdout.close()
-        # Make original_stdout_fd point to the same file as to_fd
-        os.dup2(to_fd, original_stdout_fd)
-        # Create a new sys.stdout that points to the redirected fd
-        sys.stdout = io.TextIOWrapper(os.fdopen(original_stdout_fd, 'wb'))
-
-    # Save a copy of the original stdout fd in saved_stdout_fd
-    saved_stdout_fd = os.dup(original_stdout_fd)
-    try:
-        # Create a temporary file and redirect stdout to it
-        tfile = tempfile.TemporaryFile(mode='w+b')
-        _redirect_stdout(tfile.fileno())
-        # Yield to caller, then redirect stdout back to the saved fd
-        yield
-        _redirect_stdout(saved_stdout_fd)
-        # Copy contents of temporary file to the given stream
-        tfile.flush()
-        tfile.seek(0, io.SEEK_SET)
-        if stream:
-            stream.write(tfile.read())
-    finally:
-        tfile.close()
-        os.close(saved_stdout_fd)
 
 
 class TestDumpMethods(unittest.TestCase):
 
     def test_dppl_dump_runtime(self):
-        with stdout_redirector(None):
-            self.assertEqual(dppl.runtime.dump(), 0)
+        self.assertEqual(dppl.runtime.dump(), 0)
 
     def test_dppl_ocldrv_dump_runtime(self):
-        with stdout_redirector(None):
-            self.assertEqual(drv.runtime.dump(), 0)
-
-
-suite = unittest.TestLoader().loadTestsFromTestCase(TestDumpMethods)
-unittest.TextTestRunner(verbosity=2).run(suite)
+        self.assertEqual(drv.runtime.dump(), 0)
