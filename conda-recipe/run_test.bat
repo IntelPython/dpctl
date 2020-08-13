0792e712cab6ab5d3fb175ac602c0b7f52932bba
Sergey Pokhodenko <sergey.pokhodenko@intel.com>
2020-08-14 00:23:45 +0300

    Enable tests

--- conda-recipe/run_test.bat
+++ conda-recipe/run_test.bat
@@ -1,8 +1,13 @@
 call "%ONEAPI_ROOT%/compiler/latest/env/vars.bat"
-REM IF %ERRORLEVEL% NEQ 0 exit 1
+IF %ERRORLEVEL% NEQ 0 exit 1
+
+@echo on
 
 "%PYTHON%" -c "import dppl"
 IF %ERRORLEVEL% NEQ 0 exit 1
 
 "%PYTHON%" -c "import dppl.ocldrv"
+IF %ERRORLEVEL% NEQ 0 exit 1
+
+"%PYTHON%" -m unittest -v dppl.tests
 IF %ERRORLEVEL% NEQ 0 exit 1
