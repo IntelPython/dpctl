758aacca5058ac712058851da1a296e94a45ed61
Sergey Pokhodenko <sergey.pokhodenko@intel.com>
2020-09-16 14:34:01 +0300

    Add C-API stub library for sycl memory.

---
+++ backends/include/dppl_sycl_usm_interface.h
@@ -0,0 +1,32 @@
+//===--- dppl_sycl_usm_interface.h - DPPL-SYCL interface ---*---C++ -*---===//
+//
+//               Python Data Parallel Processing Library (PyDPPL)
+//
+// Copyright 2020 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// TODO
+///
+//===----------------------------------------------------------------------===//
+
+#pragma once
+
+#include "Support/ExternC.h"
+
+DPPL_C_EXTERN_C_BEGIN
+
+DPPL_C_EXTERN_C_END
