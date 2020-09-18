7899c0735ef319cf3714314567fc6a12adc6fc86
Diptorup Deb <diptorup@cs.unc.edu>
2020-09-17 22:15:30 -0500

    Add a kernel submission interface.

--- backends/include/dppl_sycl_kernel_interface.h
+++ backends/include/dppl_sycl_kernel_interface.h
@@ -38,6 +38,92 @@
 DPPL_C_EXTERN_C_BEGIN
 
 /*!
+ * @brief Enum of currently support types for kernel arguments.
+ *
+ * \todo Add support for sycl::buffer
+ *
+ */
+enum DPPLArgTypes
+{
+    CHAR,
+    SIGNED_CHAR,
+    UNSIGNED_CHAR,
+    SHORT,
+    INT,
+    UNSIGNED_INT,
+    LONG,
+    UNSIGNED_LONG,
+    LONG_LONG,
+    UNSIGNED_LONG_LONG,
+    SIZE_T,
+    FLOAT,
+    DOUBLE,
+    LONG_DOUBLE,
+    CHAR_P,
+    SIGNED_CHAR_P,
+    UNSIGNED_CHAR_P,
+    SHORT_P,
+    INT_P,
+    UNSIGNED_INT_P,
+    LONG_P,
+    UNSIGNED_LONG_P,
+    LONG_LONG_P,
+    UNSIGNED_LONG_LONG_P,
+    SIZE_T_P,
+    FLOAT_P,
+    DOUBLE_P,
+    LONG_DOUBLE_P
+};
+
+/*!
+ * @brief A union representing an OpenCL kernel argument that is either a basic
+ * C type of a pointer of the type.
+ *
+ */
+union DPPLArgValue
+{
+    char                char_arg;
+    signed char         schar_arg;
+    unsigned char       uchar_arg;
+    short               short_arg;
+    int                 int_arg;
+    unsigned int        uint_arg;
+    long                long_arg;
+    unsigned long       ulong_arg;
+    long long           longlong_arg;
+    unsigned long long  ulonglong_arg;
+    size_t              size_t_arg;
+    float               float_arg;
+    double              double_arg;
+    long double         longdouble_arg;
+    char                *char_p_arg;
+    signed char         *schar_p_arg;
+    unsigned char       *uchar_p_arg;
+    short               *short_p_arg;
+    int                 *int_p_arg;
+    unsigned int        *uint_p_arg;
+    long                *long_p_arg;
+    unsigned long       *ulong_p_arg;
+    long long           *longlong_p_arg;
+    unsigned long long  *ulonglong_p_arg;
+    size_t              *size_t_p_arg;
+    float               *float_p_arg;
+    double              *double_p_arg;
+    long double         *longdouble_p_arg;
+};
+
+/*!
+ * @brief The tagged union is used to pass through OpenCL kernel arguments to
+ * Sycl.
+ *
+ */
+struct DPPLKernelArg
+{
+    enum DPPLArgTypes argType;
+    union DPPLArgValue argVal;
+};
+
+/*!
  * @brief Create a Sycl Kernel from an OpenCL SPIR-V binary
  *
  * Sycl 1.2 does expose any method to create a sycl::program from a SPIR-V IL
@@ -65,35 +151,69 @@ DPPLKernel_CreateKernelFromSpirv (__dppl_keep const DPPLSyclContextRef Ctx,
 /*!
  * @brief Returns a C string for the kernel name.
  *
- * @param    Kernel         DPPLSyclKernelRef pointer to an OpenCL
+ * @param    KRef           DPPLSyclKernelRef pointer to an OpenCL
  *                          interoperability kernel.
  * @return   If a kernel name exists then returns it as a C string, else
  *           returns a nullptr.
  */
 DPPL_API
 __dppl_give const char*
-DPPLKernel_GetFunctionName (__dppl_keep const DPPLSyclKernelRef Kernel);
+DPPLKernel_GetFunctionName (__dppl_keep const DPPLSyclKernelRef KRef);
 
 /*!
  * @brief Returns the number of arguments for the OpenCL kernel.
  *
- * @param    Kernel         DPPLSyclKernelRef pointer to an OpenCL
+ * @param    KRef           DPPLSyclKernelRef pointer to an OpenCL
  *                          interoperability kernel.
  * @return   Returns the number of arguments for the OpenCL interoperability
  *           kernel.
  */
 DPPL_API
 size_t
-DPPLKernel_GetNumArgs (__dppl_keep const DPPLSyclKernelRef Kernel);
+DPPLKernel_GetNumArgs (__dppl_keep const DPPLSyclKernelRef KRef);
 
 /*!
  * @brief Deletes the DPPLSyclKernelRef after casting it to a sycl::kernel.
  *
- * @param    Kernel         DPPLSyclKernelRef pointer to an OpenCL
+ * @param    KRef           DPPLSyclKernelRef pointer to an OpenCL
  *                          interoperability kernel.
  */
 DPPL_API
 void
-DPPLKernel_DeleteKernelRef (__dppl_take DPPLSyclKernelRef Kernel);
+DPPLKernel_DeleteKernelRef (__dppl_take DPPLSyclKernelRef KRef);
+
+
+/*!
+ * @brief Submits the kernel to the specified queue using give arguments.
+ *
+ * A wrapper over sycl::queue.submit(). The function takes an OpenCL
+ * interoperability kernel, the kernel arguments, and a sycl queue as input
+ * arguments. The kernel arguments are passed in as an array of the
+ * DPPLKernelArg tagged union.
+ *
+ * \todo sycl::buffer arguments are not supported yet.
+ *
+ * @param    KRef           Opaque pointer to a OpenCL interoperability kernel
+ *                          wrapped inside a sycl::kernel.
+ * @param    QRef           Opaque pointer to the sycl::queue where the kernel
+ *                          will be enqueued.
+ * @param    Args           An array of the DPPLKernelArg tagged union type that
+ *                          represents the kernel arguments for the kernel.
+ * @param    NArgs          The number of kernel arguments (size of Args array).
+ * @param    Range          Array storing the range dimensions that can have a
+ *                          maximum size of three. Note the number of values
+ *                          in the array depends on the number of dimensions.
+ * @param    NDims          Number of dimensions in the range (size of Range).
+ * @return   A opaque pointer to the sycl::event returned by the
+ *           sycl::queue.submit() function.
+ */
+DPPL_API
+DPPLSyclEventRef
+DPPLKernel_Submit (__dppl_keep DPPLSyclKernelRef KRef,
+                   __dppl_keep DPPLSyclQueueRef QRef,
+                   __dppl_keep DPPLKernelArg *Args,
+                   size_t NArgs,
+                   size_t Range[3],
+                   size_t NDims);
 
 DPPL_C_EXTERN_C_END
