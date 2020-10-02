c0acc928c083ea0bc62a37b006c5f8f108e302cb
Sergey Pokhodenko <sergey.pokhodenko@intel.com>
2020-10-02 18:37:57 -0500

    Use CMake FindPython for finding Python and NumPy include dirs

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -19,9 +19,6 @@ pushd build_cmake
 INSTALL_PREFIX=`pwd`/../install
 rm -rf ${INSTALL_PREFIX}
 
-PYTHON_INC=`${PYTHON} -c "import distutils.sysconfig;                  \
-                        print(distutils.sysconfig.get_python_inc())"`
-NUMPY_INC=`${PYTHON} -c "import numpy; print(numpy.get_include())"`
 DPCPP_ROOT=${ONEAPI_ROOT}/compiler/latest/linux/
 
 cmake                                                       \
@@ -29,8 +26,6 @@ cmake                                                       \
     -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX}                \
     -DCMAKE_PREFIX_PATH=${INSTALL_PREFIX}                   \
     -DDPCPP_ROOT=${DPCPP_ROOT}                              \
-    -DPYTHON_INCLUDE_DIR=${PYTHON_INC}                      \
-    -DNUMPY_INCLUDE_DIR=${NUMPY_INC}                        \
     ../backends
 
 make -j 4 && make check & make install
