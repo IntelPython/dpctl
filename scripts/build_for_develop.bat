a05327d6fc3d7c3d17902b9601fc849a09473a70
Oleksandr Pavlyk <oleksandr.pavlyk@intel.com>
2020-10-28 15:00:27 -0500

    further changes to build_for_devel.bat - fixed typo

--- scripts/build_for_develop.bat
+++ scripts/build_for_develop.bat
@@ -3,9 +3,6 @@ IF ERRORLEVEL 1 exit /b 1
 REM conda uses %ERRORLEVEL% but FPGA scripts can set it. So it should be reseted.
 set ERRORLEVEL=
 
-set "CC=clang-cl.exe"
-set "CXX=dpcpp.exe"
-
 rmdir /S /Q build_cmake
 mkdir build_cmake
 
@@ -23,15 +20,17 @@ set PYTHON_INC=
 for /f "delims=" %%a in ('%CONDA_PREFIX%\python.exe -c "import distutils.sysconfig as sc; print(sc.get_python_inc())"') do @set PYTHON_INC=%%a 
 
 cmake -G Ninja ^
-    -DCMAKE_BUILD_TYPE=Debug ^
-    "-DCMAKE_CXX_FLAGS=-Wno-unused-function" ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    "-DCMAKE_CXX_FLAGS=-Wno-unused-function /EHa" ^
     "-DCMAKE_INSTALL_PREFIX=%INSTALL_PREFIX%" ^
     "-DCMAKE_PREFIX_PATH=%INSTALL_PREFIX%" ^
     "-DDPCPP_ROOT=%DPCPP_ROOT%" ^
+    "-DCMAKE_C_COMPILER:PATH=%DPCPP_ROOT%\bin\clang-cl.exe" ^
+    "-DCMAKE_CXX_COMPILER:PATH=%DPCPP_ROOT%\bin\dpcpp.exe" ^
     "-DPYTHON_INCLUDE_DIR=%PYTHON_INC%" ^
     "-DGTEST_INCLUDE_DIR=%CONDA_PREFIX%\Library\include" ^
     "-DGTEST_LIB_DIR=%CONDA_PREFIX%\Library\lib" ^
-    "-DNUMPY_INCLUDE_DIR=%NUMPY_DIR%" ^
+    "-DNUMPY_INCLUDE_DIR=%NUMPY_INC%" ^
     "%cd%\..\backends"
 IF %ERRORLEVEL% NEQ 0 exit /b 1
 
@@ -53,6 +52,8 @@ xcopy backends\include dpctl\include /E /Y
 REM required by _sycl_core(dpctl)
 set "DPPL_SYCL_INTERFACE_LIBDIR=dpctl"
 set "DPPL_SYCL_INTERFACE_INCLDIR=dpctl\include"
+set "CC=clang-cl.exe"
+set "CXX=dpcpp.exe"
 
 python setup.py clean --all
 python setup.py build_ext --inplace develop
