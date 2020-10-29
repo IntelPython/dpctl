6bcaf0d18d0045eb4a7678cf33149a5ee4d9dc15
Diptorup Deb <diptorup.deb@intel.com>
2020-10-29 09:00:51 -0500

    make black happy.

--- setup.py
+++ setup.py
@@ -126,12 +126,11 @@ def extensions():
         runtime_library_dirs = []
 
     extension_args = {
-        "depends": [
-            dppl_sycl_interface_include,
-        ],
+        "depends": [dppl_sycl_interface_include,],
         "include_dirs": [np.get_include(), dppl_sycl_interface_include],
-        "extra_compile_args": eca + get_other_cxxflags() +
-                              get_suppressed_warning_flags(),
+        "extra_compile_args": eca
+        + get_other_cxxflags()
+        + get_suppressed_warning_flags(),
         "extra_link_args": ela,
         "libraries": libs,
         "library_dirs": librarys,
@@ -142,16 +141,12 @@ def extensions():
     extensions = [
         Extension(
             "dpctl._sycl_core",
-            [
-                os.path.join("dpctl", "_sycl_core.pyx"),
-            ],
+            [os.path.join("dpctl", "_sycl_core.pyx"),],
             **extension_args
         ),
         Extension(
             "dpctl.memory._memory",
-            [
-                os.path.join("dpctl", "memory", "_memory.pyx"),
-            ],
+            [os.path.join("dpctl", "memory", "_memory.pyx"),],
             **extension_args
         ),
     ]
