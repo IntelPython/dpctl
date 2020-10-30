8b61aff3be0f5f70fc01cf0d0d11c7c647c52852
Diptorup Deb <3046810+diptorupd@users.noreply.github.com>
2020-10-30 16:12:17 -0500

    Update bld.bat

--- conda-recipe/bld.bat
+++ conda-recipe/bld.bat
@@ -1,6 +1,6 @@
 call "%ONEAPI_ROOT%\compiler\latest\env\vars.bat"
 IF %ERRORLEVEL% NEQ 0 (
-	echo "oneAPI compiler activation failed%"
+	echo "oneAPI compiler activation failed"
 	exit /b 1
 )
 REM conda uses %ERRORLEVEL% but FPGA scripts can set it. So it should be reseted.
