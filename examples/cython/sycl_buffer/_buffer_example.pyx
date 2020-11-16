d541443a8181d48b8162be293d898f55ce2ec416
Oleksandr Pavlyk <oleksandr.pavlyk@intel.com>
2020-11-16 16:45:37 -0600

    Extended sycl_buffer example to implement column-wise summation without MKL

--- examples/cython/sycl_buffer/_buffer_example.pyx
+++ examples/cython/sycl_buffer/_buffer_example.pyx
@@ -6,8 +6,9 @@ import dpctl
 
 cdef extern from "use_sycl_buffer.h":
     int c_columnwise_total(c_dpctl.DPPLSyclQueueRef q, size_t n, size_t m, double *m, double *ct) nogil
+    int c_columnwise_total_no_mkl(c_dpctl.DPPLSyclQueueRef q, size_t n, size_t m, double *m, double *ct) nogil
 
-def columnwise_total(double[:, ::1] v):
+def columnwise_total(double[:, ::1] v, method='mkl'):
     cdef cnp.ndarray res_array = np.empty((v.shape[1],), dtype='d')
     cdef double[::1] res_memslice = res_array
     cdef int ret_status
@@ -17,7 +18,11 @@ def columnwise_total(double[:, ::1] v):
     q = c_dpctl.get_current_queue()
     q_ref = q.get_queue_ref()
 
-    with nogil:
-        ret_status = c_columnwise_total(q_ref, v.shape[0], v.shape[1], &v[0,0], &res_memslice[0])
+    if method == 'mkl':
+        with nogil:
+            ret_status = c_columnwise_total(q_ref, v.shape[0], v.shape[1], &v[0,0], &res_memslice[0])
+    else:
+        with nogil:
+            ret_status = c_columnwise_total_no_mkl(q_ref, v.shape[0], v.shape[1], &v[0,0], &res_memslice[0])        
 
     return res_array
--- examples/cython/sycl_buffer/use_sycl_buffer.h
+++ examples/cython/sycl_buffer/use_sycl_buffer.h
@@ -3,3 +3,5 @@
 
 extern int c_columnwise_total(
     DPPLSyclQueueRef q, size_t n, size_t m, double *mat, double *ct);
+extern int c_columnwise_total_no_mkl(
+    DPPLSyclQueueRef q, size_t n, size_t m, double *mat, double *ct);
