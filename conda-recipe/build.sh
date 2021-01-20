bb9936be31268f02c08f21f0753bf3a78cb8b211
Vyacheslav Smirnov <vyacheslav.s.smirnov@intel.com>
2021-01-20 12:21:12 +0300

    Update linux build tah for wheels

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -13,7 +13,12 @@ ${PYTHON} setup.py clean --all
 ${PYTHON} setup.py install
 
 # Build wheel package
+if [ "$CONDA_PY" == "36" ]; then
+    WHEELS_BUILD_ARGS="-p manylinux1_x86_64"
+else
+    WHEELS_BUILD_ARGS="-p manylinux2014_x86_64"
+fi
 if [ -n "${WHEELS_OUTPUT_FOLDER}" ]; then
-    $PYTHON setup.py bdist_wheel -p manylinux1_x86_64
+    $PYTHON setup.py bdist_wheel ${WHEELS_BUILD_ARGS}
     cp dist/dpctl*.whl ${WHEELS_OUTPUT_FOLDER}
 fi
