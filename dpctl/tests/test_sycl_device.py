2f67b7ea6780243ea788237ab08fb685c51547ab
Diptorup Deb <diptorup.deb@intel.com>
2021-02-17 13:15:20 -0600

    Change the way tests are recorded by pytest.

--- dpctl/tests/test_sycl_device.py
+++ dpctl/tests/test_sycl_device.py
@@ -52,6 +52,85 @@ list_of_invalid_filter_selectors = [
     "level_zero:cpu:0",
 ]
 
+# Unit test cases that will be run for every device
+def check_get_max_compute_units(device):
+    max_compute_units = device.get_max_compute_units()
+    assert max_compute_units > 0
+
+def check_get_max_work_item_dims(device):
+    max_work_item_dims = device.get_max_work_item_dims()
+    assert max_work_item_dims > 0
+
+def check_get_max_work_item_sizes(device):
+    max_work_item_sizes = device.get_max_work_item_sizes()
+    for size in max_work_item_sizes:
+        assert size is not None
+
+def check_get_max_work_group_size(device):
+    max_work_group_size = device.get_max_work_group_size()
+    # Special case for FPGA simulator
+    if device.is_accelerator():
+        assert max_work_group_size >= 0
+    else:
+        assert max_work_group_size > 0
+
+def check_get_max_num_sub_groups(device):
+    max_num_sub_groups = device.get_max_num_sub_groups()
+    # Special case for FPGA simulator
+    if device.is_accelerator():
+        assert max_num_sub_groups >= 0
+    else:
+        assert max_num_sub_groups > 0
+
+def check_has_int64_base_atomics(device):
+    try:
+        device.has_int64_base_atomics()
+    except Exception:
+        pytest.fail("has_int64_base_atomics call failed")
+
+def check_has_int64_extended_atomics(device):
+    try:
+        device.has_int64_extended_atomics()
+    except Exception:
+        pytest.fail("has_int64_extended_atomics call failed")
+
+def check_is_accelerator(device):
+    try:
+        device.is_accelerator()
+    except Exception:
+        pytest.fail("is_accelerator call failed")
+
+def check_is_cpu(device):
+    try:
+        device.is_cpu()
+    except Exception:
+        pytest.fail("is_cpu call failed")
+
+def check_is_gpu(device):
+    try:
+        device.is_gpu()
+    except Exception:
+        pytest.fail("is_gpu call failed")
+
+def check_is_host(device):
+    try:
+        device.is_host()
+    except Exception:
+        pytest.fail("is_hostcall failed")
+
+list_of_checks = [
+check_get_max_compute_units,
+check_get_max_work_item_dims,
+check_get_max_work_item_sizes,
+check_get_max_work_group_size,
+check_get_max_num_sub_groups,
+check_has_int64_base_atomics,
+check_has_int64_extended_atomics,
+check_is_accelerator,
+check_is_cpu,
+check_is_gpu,
+check_is_host,
+]
 
 @pytest.fixture(params=list_of_valid_filter_selectors)
 def valid_filter(request):
@@ -67,109 +146,37 @@ def invalid_filter(request):
 def device_selector(request):
     return request.param
 
-
-class DeviceTestFunctions:
-    def __init__(self, device):
-        self.check_get_max_compute_units(device)
-        self.check_get_max_work_item_dims(device)
-        self.check_get_max_work_item_sizes(device)
-        self.check_get_max_work_group_size(device)
-        self.check_get_max_num_sub_groups(device)
-        self.check_has_int64_base_atomics(device)
-        self.check_has_int64_extended_atomics(device)
-
-    def check_get_max_compute_units(self, device):
-        max_compute_units = device.get_max_compute_units()
-        assert max_compute_units > 0
-
-    def check_get_max_work_item_dims(self, device):
-        max_work_item_dims = device.get_max_work_item_dims()
-        assert max_work_item_dims > 0
-
-    def check_get_max_work_item_sizes(self, device):
-        max_work_item_sizes = device.get_max_work_item_sizes()
-        for size in max_work_item_sizes:
-            assert size is not None
-
-    def check_get_max_work_group_size(self, device):
-        max_work_group_size = device.get_max_work_group_size()
-        # Special case for FPGA simulator
-        if device.is_accelerator():
-            assert max_work_group_size >= 0
-        else:
-            assert max_work_group_size > 0
-
-    def check_get_max_num_sub_groups(self, device):
-        max_num_sub_groups = device.get_max_num_sub_groups()
-        # Special case for FPGA simulator
-        if device.is_accelerator():
-            assert max_num_sub_groups >= 0
-        else:
-            assert max_num_sub_groups > 0
-
-    def check_has_int64_base_atomics(self, device):
-        try:
-            device.has_int64_base_atomics()
-        except Exception:
-            pytest.fail("has_int64_base_atomics call failed")
-
-    def check_has_int64_extended_atomics(self, device):
-        try:
-            device.has_int64_extended_atomics()
-        except Exception:
-            pytest.fail("has_int64_extended_atomics call failed")
-
-    def check_is_accelerator(self, device):
-        try:
-            device.is_accelerator()
-        except Exception:
-            pytest.fail("is_accelerator call failed")
-
-    def check_is_cpu(self, device):
-        try:
-            device.is_cpu()
-        except Exception:
-            pytest.fail("is_cpu call failed")
-
-    def check_is_gpu(self, device):
-        try:
-            device.is_gpu()
-        except Exception:
-            pytest.fail("is_gpu call failed")
-
-    def check_is_host(self, device):
-        try:
-            device.is_host()
-        except Exception:
-            pytest.fail("is_hostcall failed")
+@pytest.fixture(params=list_of_checks)
+def check(request):
+    return request.param
 
 
-def test_standard_selectors(device_selector):
+def test_standard_selectors(device_selector, check):
     """Tests if the standard SYCL device_selectors are able to select a
     device.
     """
     try:
         device = device_selector()
-        DeviceTestFunctions(device)
+        check(device)
     except ValueError:
         pytest.skip()
 
 
-def test_current_device():
+def test_current_device(check):
     """Test is the device for the current queue is valid."""
     try:
         q = dpctl.get_current_queue()
     except Exception:
         pytest.fail("Encountered an exception inside get_current_queue().")
     device = q.get_sycl_device()
-    DeviceTestFunctions(device)
+    check(device)
 
 
-def test_valid_filter_selectors(valid_filter):
+def test_valid_filter_selectors(valid_filter, check):
     """Tests if we can create a SyclDevice using a supported filter selector string."""
     try:
         device = dpctl.SyclDevice(valid_filter)
-        DeviceTestFunctions(device)
+        check(device)
     except ValueError:
         pytest.fail("Failed to create device with supported filter")
 
