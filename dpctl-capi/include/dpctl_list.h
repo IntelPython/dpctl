f799f55810a3717b7235e4c80f38426580e2f717
Diptorup Deb <diptorup.deb@intel.com>
2021-02-19 15:05:47 -0600

    WIP macros to generate vector types and functions.

---
+++ dpctl-capi/include/dpctl_list.h
@@ -0,0 +1,68 @@
+//===-- dpctl_list.h - Defines macros for opaque vector types      -*-C++-*- =//
+//
+//                      Data Parallel Control (dpCtl)
+//
+// Copyright 2020-2021 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// A set of helper macros are defined here to create opaque lists (implemented
+/// using std::vector) and helper functions of any DPCTL type.
+///
+//===----------------------------------------------------------------------===//
+
+#pragma once
+
+#include "Support/DllExport.h"
+#include "Support/ExternC.h"
+#include "Support/MemOwnershipAttrs.h"
+#include "dpctl_data_types.h"
+
+DPCTL_C_EXTERN_C_BEGIN
+
+#define DPCTL_DECLARE_VECTOR_TYPE(EL)                                          \
+    typedef struct DPCTL##EL##Vector *DPCTL##EL##VectorRef;
+
+#define DPCTL_DECLARE_VECTOR_FN(EL)                                            \
+    DPCTL_API                                                                  \
+    __dpctl_give DPCTL##EL##VectorRef DPCTL##EL##Vector_Create();              \
+                                                                               \
+    DPCTL_API                                                                  \
+    void DPCTL##EL##Vector_Delete(__dpctl_take DPCTL##EL##VectorRef Ref);      \
+                                                                               \
+    DPCTL_API                                                                  \
+    void DPCTL##EL##Vector_Clear(__dpctl_keep DPCTL##EL##VectorRef Ref);       \
+                                                                               \
+    DPCTL_API                                                                  \
+    size_t DPCTL##EL##Vector_Size(__dpctl_keep DPCTL##EL##VectorRef Ref);      \
+                                                                               \
+    DPCTL_API                                                                  \
+    size_t DPCTL##EL##Vector_GetAt(__dpctl_keep DPCTL##EL##VectorRef Ref,      \
+                                   size_t index);                              \
+                                                                               \
+    DPCTL_API                                                                  \
+    size_t DPCTL##EL##Vector_Get(__dpctl_keep DPCTL##EL##VectorRef Ref,        \
+                                 size_t index);                                \
+                                                                               \
+    DPCTL_API                                                                  \
+    void DPCTL##EL##Vector_PushBack(__dpctl_keep DPCTL##EL##VectorRef Ref,     \
+                                    __dpctl_take DPCTLSycl##EL##Ref ELRef);
+
+#define DPCTL_DECLARE_VECTOR(EL)                                               \
+    DPCTL_DECLARE_VECTOR_TYPE(EL)                                              \
+    DPCTL_DECLARE_VECTOR_FN(EL)
+
+DPCTL_C_EXTERN_C_END
---
+++ dpctl-capi/source/dpctl_list_templ.cpp
@@ -0,0 +1,19 @@
+
+#include <vector>
+
+__dpctl_give VECTOR(EL) FN(EL, _Create)() {}
+
+void FN(EL, _Delete)(__dpctl_take VECTOR(EL) VRef) {}
+
+void FN(EL, _Clear)(__dpctl_keep VECTOR(EL) VRef) {}
+
+size_t FN(EL, _Size)(__dpctl_keep VECTOR(EL) VRef) {}
+
+size_t FN(EL, _GetAt)(__dpctl_keep VECTOR(EL) VRef, size_t index) {}
+
+size_t FN(EL, _Get)(__dpctl_keep VECTOR(EL) VRef, size_t index) {}
+
+void FN(EL, _PushBack)(__dpctl_keep VECTOR(EL) VRef,
+                       __dpctl_take SYCLREF(EL) ELRef)
+{
+}
