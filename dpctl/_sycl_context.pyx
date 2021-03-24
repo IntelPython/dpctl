79f31975b93256923946f4ba73e066107eebbe3a
Oleksandr Pavlyk <oleksandr.pavlyk@intel.com>
2021-03-24 18:55:21 -0500

    Added constructor for SyclContext

--- dpctl/_sycl_context.pyx
+++ dpctl/_sycl_context.pyx
@@ -24,9 +24,22 @@ from __future__ import print_function
 import logging
 from ._backend cimport (
     DPCTLSyclContextRef,
+    DPCTLSyclDeviceRef,
+    DPCTLContext_Create,
+    DPCTLContext_CreateFromDevices,
+    DPCTLContext_Copy,
     DPCTLContext_Delete,
     DPCTLContext_AreEq,
+    DPCTLDevice_Delete,
+    DPCTLDevice_Copy,
+    DPCTLDeviceVectorRef,
+    DPCTLDeviceVector_CreateFromArray,
+    DPCTLDeviceVector_Delete,
+    error_handler_callback
 )
+from ._sycl_queue cimport default_async_error_handler
+from ._sycl_device cimport SyclDevice
+from cpython.mem cimport PyMem_Malloc, PyMem_Free
 
 __all__ = [
     "SyclContext",
@@ -34,18 +47,105 @@ __all__ = [
 
 _logger = logging.getLogger(__name__)
 
+cdef class _SyclContext:
+    """ Data owner for SyclContext
+    """
 
-cdef class SyclContext:
+    def __dealloc__(self):
+        DPCTLContext_Delete(self._ctxt_ref)
+
+
+cdef class SyclContext(_SyclContext):
     """ Python wrapper class for cl::sycl::context.
     """
+    
+    @staticmethod
+    cdef void _init_helper(_SyclContext context, DPCTLSyclContextRef CRef):
+         context._ctxt_ref = CRef
+    
     @staticmethod
     cdef SyclContext _create (DPCTLSyclContextRef ctxt):
-        cdef SyclContext ret = SyclContext.__new__(SyclContext)
-        ret._ctxt_ref = ctxt
-        return ret
+        cdef _SyclContext ret = <_SyclContext>_SyclContext.__new__(_SyclContext)
+        SyclContext._init_helper(ret, ctxt)
+        return SyclContext(ret)
 
-    def __dealloc__ (self):
-        DPCTLContext_Delete(self._ctxt_ref)
+    cdef int _init_from__SyclContext(self, _SyclContext other):
+        self._ctxt_ref = DPCTLContext_Copy(other._ctxt_ref)
+        if (self._ctxt_ref is NULL):
+            return -1
+        return 0
+
+    cdef int _init_from_one_device(self, SyclDevice device, int props):
+        cdef DPCTLSyclDeviceRef DRef = device.get_device_ref()
+        cdef error_handler_callback * eh_callback = \
+            <error_handler_callback *>&default_async_error_handler
+        cdef DPCTLSyclContextRef CRef = DPCTLContext_Create(DRef, eh_callback, props)
+        if (CRef is NULL):
+            return -1
+        SyclContext._init_helper(<_SyclContext> self, CRef)
+        return 0
+
+    cdef int _init_from_devices(self, object devices, int props):
+        cdef int num_devices = len(devices)
+        cdef int i = 0
+        cdef int j
+        cdef size_t num_bytes
+        cdef DPCTLDeviceVectorRef DVRef
+        cdef error_handler_callback * eh_callback = \
+            <error_handler_callback *>&default_async_error_handler
+        cdef DPCTLSyclContextRef CRef = NULL
+        cdef DPCTLSyclDeviceRef *elems
+
+        if num_devices > 0:
+            num_bytes = num_devices * sizeof(DPCTLSyclDeviceRef *)
+            elems = <DPCTLSyclDeviceRef *>PyMem_Malloc(num_bytes)
+            if (elems is NULL):
+                return -3
+            for dev in devices:
+                if not isinstance(dev, SyclDevice):
+                    elems[i] = NULL
+                else:
+                    elems[i] = DPCTLDevice_Copy((<SyclDevice>dev).get_device_ref())
+                if (elems[i] is NULL):
+                    for j in range(0, i):
+                        DPCTLDevice_Delete(elems[j])
+                    PyMem_Free(elems)
+                    return -4
+                i = i + 1
+            DVRef = DPCTLDeviceVector_CreateFromArray(num_devices, elems)
+            if (DVRef is NULL):
+                for j in range(num_devices):
+                    DPCTLDevice_Delete(elems[j])
+                PyMem_Free(elems)
+                return -5
+            PyMem_Free(elems)
+        else:
+            return -2
+        DPCTLContext_CreateFromDevices(DVRef, eh_callback, props)
+        DPCTLDeviceVector_Delete(DVRef)
+        if (CRef is NULL):
+            return -1
+        SyclContext._init_helper(<_SyclContext> self, CRef)
+        return 0
+
+    def __cinit__(self, arg=None):
+        """ SyclContext() - create a context for a default device
+            SyclContext(filter_selector_string) - create a context for specified device
+            SyclContext(SyclDevice_instance) - create a context for the given device
+            SyclContext((dev1, dev2, ...)) - create a context for given set of devices
+        """
+        cdef int ret = 0
+        if isinstance(arg, _SyclContext):
+            ret = self._init_from__SyclContext(<_SyclContext> arg)
+        elif isinstance(arg, SyclDevice):
+            ret = self._init_from_one_device(<SyclDevice> arg, 0)
+        elif isinstance(arg, (list, tuple)) and all([isinstance(argi, SyclDevice) for argi in arg]):
+            ret = self._init_from_devices(arg, 0)
+        else:
+            dev = SyclDevice(arg)
+            ret = self._init_from_one_device(<SyclDevice> dev, 0)
+        if (ret < 0):
+            raise ValueError(ret)
 
     cpdef bool equals (self, SyclContext ctxt):
         """ Returns true if the SyclContext argument has the same _context_ref
