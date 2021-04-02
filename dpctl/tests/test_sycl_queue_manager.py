992d5e159d050276c9e83f5228ce8ac1472cddd6
Sergey Pokhodenko <sergey.pokhodenko@intel.com>
2021-04-03 01:35:35 +0300

    Fix tests which fail if GPU is not available (only CPU is available) (#359)
    * Skip tests required OpenCL if GPU is not available
    * Skip tests for SyclContext which require GPU

--- dpctl/tests/test_sycl_queue_manager.py
+++ dpctl/tests/test_sycl_queue_manager.py
@@ -43,6 +43,7 @@ class TestIsInDeviceContext(unittest.TestCase):
 
 
 @unittest.skipIf(not has_sycl_platforms(), "No SYCL platforms available")
+@unittest.skipUnless(has_gpu(), "No OpenCL GPU queues available")
 class TestGetCurrentDevice(unittest.TestCase):
     def test_get_current_device_type_outside_device_ctxt(self):
         self.assertNotEqual(dpctl.get_current_device_type(), None)
