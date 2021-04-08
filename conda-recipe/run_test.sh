0b75b5a656538788f88c966f2fb03b8569c194d1
vlad-perevezentsev <vladislav.perevezentsev@intel.com>
2021-04-08 04:17:02 -0500

    Use pytest-cov

--- conda-recipe/run_test.sh
+++ conda-recipe/run_test.sh
@@ -6,4 +6,4 @@ set -e
 source ${ONEAPI_ROOT}/compiler/latest/env/vars.sh || true
 
 ${PYTHON} -c "import dpctl"
-coverage run -m --source=dpctl --branch --omit=*/dpctl/tests/*,*/dpctl/_version.py pytest -q -ra --disable-warnings --pyargs dpctl -vv && coverage report -m
+pytest -q -ra --disable-warnings --cov dpctl --cov-report term-missing --pyargs dpctl -vv
