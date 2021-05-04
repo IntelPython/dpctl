92079c29a7a5d83aba2f549054f6137b663659ff
diptorupd <diptorup.deb@intel.com>
2021-05-04 11:27:22 -0500

    Changes to C API build scripts to allow using the open source dpcpp compiler.
       - FindDPCPP.cmake is now renamed to FindIntelSycl.cmake.
       - llvm-cov and llvm-profdata now have their own cmake modules
         and the minimum version required is 11.0.0 for these tools.
       - CMAKE_C_COMPILER and CMAKE_CXX_COMPILER are now set by the
         FindIntelSycl.cmake module and do not need to be set manually.
       - A new CMAKE flag DPCTL_CUSTOM_DPCPP_INSTALL_DIR is now available
         that makes it possible to build the DPCTLSyclInterface with a
         custom dpcpp compiler.
       - Formatting changes to CMakeLists.txt files.
       - A new helper script to build the DPCTLSyclInterface library using a
         custom dpcpp.

---
+++ dpctl-capi/dbg_build.bat
@@ -0,0 +1,43 @@
+@REM check if oneAPI has been activated, only try activating if not
+dpcpp.exe --version >nul 2>&1
+if errorlevel 1 (
+    set ERRORLEVEL=
+    call "%ONEAPI_ROOT%\compiler\latest\env\vars.bat"
+    if errorlevel 1 exit 1
+)
+@REM conda uses %ERRORLEVEL% but FPGA scripts can set it. So it should be reseted.
+set ERRORLEVEL=
+
+rmdir /S /Q build
+mkdir build
+
+rmdir /S /Q install
+mkdir install
+cd install
+set "INSTALL_PREFIX=%cd%"
+
+cd ..\build
+
+set "DPCPP_HOME=%ONEAPI_ROOT%\compiler\latest\windows"
+
+cmake -G Ninja ^
+    -DCMAKE_BUILD_TYPE=Debug ^
+    "-DCMAKE_CXX_FLAGS=-Wno-unused-function /EHsc" ^
+    "-DCMAKE_INSTALL_PREFIX=%INSTALL_PREFIX%" ^
+    "-DCMAKE_PREFIX_PATH=%INSTALL_PREFIX%" ^
+    "-DDPCPP_INSTALL_DIR=%DPCPP_HOME%" ^
+    "-DCMAKE_C_COMPILER:PATH=%DPCPP_HOME%\bin\clang-cl.exe" ^
+    "-DCMAKE_CXX_COMPILER:PATH=%DPCPP_HOME%\bin\dpcpp.exe" ^
+    "-DCMAKE_LINKER:PATH=%DPCPP_HOME%\bin\lld-link.exe" ^
+    "-DDPCTL_BUILD_CAPI_TESTS=ON" ^
+    ".."
+if errorlevel 1 exit 1
+
+ninja -n
+if errorlevel 1 exit 1
+@REM ninja check
+@REM IF %ERRORLEVEL% NEQ 0 exit /b 1
+ninja install
+if errorlevel 1 exit 1
+
+cd ..
