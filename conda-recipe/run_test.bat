a005320225e9f6d1ff2c1b80ae86be82431d4cd8
Oleksandr Pavlyk <oleksandr.pavlyk@intel.com>
2021-06-21 10:32:31 -0500

    Test runners should not be activating oneAPI. More recent run-times are being installed into Python environment and should be used instead of those in the compiler installation

--- conda-recipe/run_test.bat
+++ conda-recipe/run_test.bat
@@ -1,11 +1,3 @@
-call "%ONEAPI_ROOT%\compiler\latest\env\vars.bat"
-if errorlevel 1 (
-	echo "oneAPI compiler activation failed%"
-	exit /b 1
-)
-REM conda uses %ERRORLEVEL% but FPGA scripts can set it. So it should be reseted.
-set ERRORLEVEL=
-
 @echo on
 
 "%PYTHON%" -c "import dpctl"
