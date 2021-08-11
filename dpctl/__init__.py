a20118467e16ea8429b037f8858dbd902e93a4dc
vlad-perevezentsev <vladislav.perevezentsev@intel.com>
2021-08-11 15:39:24 -0500

    Add a execution_status method for SyclEventRaw

--- dpctl/__init__.py
+++ dpctl/__init__.py
@@ -65,7 +65,7 @@ from dpctl._sycl_queue_manager import (
 )
 
 from ._version import get_versions
-from .enum_types import backend_type, device_type
+from .enum_types import backend_type, device_type, event_status_type
 
 __all__ = [
     "SyclContext",
@@ -113,6 +113,7 @@ __all__ += [
 __all__ += [
     "device_type",
     "backend_type",
+    "event_status_type",
 ]
 __all__ += [
     "get_include",
