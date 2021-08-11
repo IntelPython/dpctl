414de9c6830eed25f27103168f9a85f983b9e23e
vlad-perevezentsev <vladislav.perevezentsev@intel.com>
2021-08-11 18:28:33 +0300

    Add a new SyclEventRaw class (#520)

--- dpctl/__init__.py
+++ dpctl/__init__.py
@@ -46,7 +46,7 @@ from dpctl._sycl_device_factory import (
     select_gpu_device,
     select_host_device,
 )
-from dpctl._sycl_event import SyclEvent
+from dpctl._sycl_event import SyclEvent, SyclEventRaw
 from dpctl._sycl_platform import SyclPlatform, get_platforms, lsplatform
 from dpctl._sycl_queue import (
     SyclKernelInvalidRangeError,
@@ -88,6 +88,7 @@ __all__ += [
 ]
 __all__ += [
     "SyclEvent",
+    "SyclEventRaw",
 ]
 __all__ += [
     "get_platforms",
