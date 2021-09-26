30e3cc6ac1dbe96f32bfcac4b5ebfd1b81f89a67
Oleksandr Pavlyk <oleksandr.pavlyk@intel.com>
2021-09-26 12:44:21 -0500

    Added new example of Python object exposing __sycl_usm_array_interface__
    Native Pybind11-generated extension implemented Python type bound to
    DMatrix C++ class which allocates USM memory using sycl::usm_allocator.
    
    The Python object implements __sycl_usm_array_interface__, which allows
    dpctl.memory.as_usm_memory to create a view into that native USM allocation.
    The example.py modifies that memory from Python, and uses object's own
    .tolist() method to retrieve the memory using C++, demonstrating that
    values changed.
    
    Also added license missing headers to .cpp and .hpp files from other
    examples

--- examples/cython/usm_memory/sycl_blackscholes.hpp
+++ examples/cython/usm_memory/sycl_blackscholes.hpp
@@ -1,3 +1,29 @@
+//=- sycl_blackscholes.hpp - Example of SYCL code to be called from Cython  =//
+//
+//                      Data Parallel Control (dpctl)
+//
+// Copyright 2020-2021 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// This file exports C++ functions to be called from Cython-generated
+/// extensions.
+///
+//===----------------------------------------------------------------------===//
+
 #include "dpctl_sycl_types.h"
 #include <CL/sycl.hpp>
 
--- examples/pybind11/use_dpctl_syclqueue/pybind11_example.cpp
+++ examples/pybind11/use_dpctl_syclqueue/pybind11_example.cpp
@@ -1,9 +1,39 @@
+//==- pybind11_example.cpp - Example of Pybind11 extension working with  -===//
+//  dpctl Python objects.
+//
+//                      Data Parallel Control (dpctl)
+//
+// Copyright 2020-2021 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// This file implements Pybind11-generated extension exposing functions that
+/// take dpctl Python objects, such as dpctl.SyclQueue, dpctl.SyclDevice as
+/// arguments.
+///
+//===----------------------------------------------------------------------===//
+
 #include <CL/sycl.hpp>
 #include <cstdint>
 #include <pybind11/numpy.h>
 #include <pybind11/pybind11.h>
 
 // clang-format off
+// Ordering of includes is important here. dpctl_sycl_types defines types
+// used by dpctl's Python C-API headers.
 #include "dpctl_sycl_types.h"
 #include "../_sycl_queue.h"
 #include "../_sycl_queue_api.h"
