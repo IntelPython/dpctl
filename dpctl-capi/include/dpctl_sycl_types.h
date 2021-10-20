c701777276d54f9328041ac0d2d04a2a4b526b99
Diptorup Deb <diptorup.deb@intel.com>
2021-10-20 15:01:46 -0500

    Rename all SYCL opaque types.

--- dpctl-capi/include/dpctl_sycl_types.h
+++ dpctl-capi/include/dpctl_sycl_types.h
@@ -31,56 +31,117 @@ DPCTL_C_EXTERN_C_BEGIN
 
 /*!
  * @brief Opaque pointer to a ``sycl::context``
- *
+ * @deprecated Use DpctlSyclContextRef
  */
-typedef struct DPCTLOpaqueSyclContext *DPCTLSyclContextRef;
+typedef struct DPCTLOpaqueSyclContext *DPCTLSyclContextRef
+    __attribute__((deprecated("Use DpctlSyclContextRef")));
 
 /*!
  * @brief Opaque pointer to a ``sycl::device``
- *
+ * @deprecated Use DpctlSyclDeviceRef
  */
-typedef struct DPCTLOpaqueSyclDevice *DPCTLSyclDeviceRef;
+typedef struct DPCTLOpaqueSyclDevice *DPCTLSyclDeviceRef
+    __attribute__((deprecated("Use DpctlSyclDeviceRef")));
+
+/*!
+ * @brief Opaque pointer to a ``sycl::device_selector``
+ * @deprecated Use DpctlSyclDeviceSelectorRef
+ */
+typedef struct DPCTLOpaqueSyclDeviceSelector *DPCTLSyclDeviceSelectorRef
+    __attribute__((deprecated("Use DpctlSyclDeviceSelectorRef")));
+
+/*!
+ * @brief Opaque pointer to a ``sycl::event``
+ * @deprecated Use DpctlSyclEventRef
+ */
+typedef struct DPCTLOpaqueSyclEvent *DPCTLSyclEventRef
+    __attribute__((deprecated("Use DpctlSyclEventRef")));
+/*!
+ * @brief Opaque pointer to a ``sycl::kernel``
+ * @deprecated Use DpctlSyclKernelRef
+ */
+typedef struct DPCTLOpaqueSyclKernel *DPCTLSyclKernelRef
+    __attribute__((deprecated("Use DpctlSyclKernelRef")));
+
+/*!
+ * @brief Opaque pointer to a ``sycl::platform``
+ * @deprecated Use DpctlSyclPlatformRef
+ */
+typedef struct DPCTLOpaqueSyclPlatform *DPCTLSyclPlatformRef
+    __attribute__((deprecated("Use DpctlSyclPlatformRef")));
+/*!
+ * @brief Opaque pointer to a ``sycl::program``
+ * @deprecated Use DpctlSyclProgramRef
+ */
+typedef struct DPCTLOpaqueSyclProgram *DPCTLSyclProgramRef
+    __attribute__((deprecated("Use DpctlSyclProgramRef")));
+
+/*!
+ * @brief Opaque pointer to a ``sycl::queue``
+ * @deprecated Use DpctlSyclQueueRef
+ */
+typedef struct DPCTLOpaqueSyclQueue *DPCTLSyclQueueRef
+    __attribute__((deprecated("Use DpctlSyclQueueRef")));
+
+/*!
+ * @brief Used to pass a ``sycl::usm`` memory opaquely through DPCTL interfaces.
+ * @deprecated Use DpctlSyclUSMRef
+ */
+typedef struct DPCTLOpaqueSyclUSM *DPCTLSyclUSMRef
+    __attribute__((deprecated("Use DpctlSyclUSMRef")));
+
+//---------------------------- New API ---------------------------------------//
+
+/*!
+ * @brief Opaque pointer to a ``sycl::context``
+ */
+typedef struct DpctlOpaqueSyclContext *DpctlSyclContextRef;
+
+/*!
+ * @brief Opaque pointer to a ``sycl::device``
+ */
+typedef struct DpctlOpaqueSyclDevice *DpctlSyclDeviceRef;
 
 /*!
  * @brief Opaque pointer to a ``sycl::device_selector``
  *
  */
-typedef struct DPCTLOpaqueSyclDeviceSelector *DPCTLSyclDeviceSelectorRef;
+typedef struct DpctlOpaqueSyclDeviceSelector *DpctlSyclDeviceSelectorRef;
 
 /*!
  * @brief Opaque pointer to a ``sycl::event``
  *
  */
-typedef struct DPCTLOpaqueSyclEvent *DPCTLSyclEventRef;
+typedef struct DpctlOpaqueSyclEvent *DpctlSyclEventRef;
 
 /*!
  * @brief Opaque pointer to a ``sycl::kernel``
  *
  */
-typedef struct DPCTLOpaqueSyclKernel *DPCTLSyclKernelRef;
+typedef struct DpctlOpaqueSyclKernel *DpctlSyclKernelRef;
 
 /*!
  * @brief Opaque pointer to a ``sycl::platform``
  *
  */
-typedef struct DPCTLOpaqueSyclPlatform *DPCTLSyclPlatformRef;
+typedef struct DpctlOpaqueSyclPlatform *DpctlSyclPlatformRef;
 
 /*!
  * @brief Opaque pointer to a ``sycl::program``
  *
  */
-typedef struct DPCTLOpaqueSyclProgram *DPCTLSyclProgramRef;
+typedef struct DpctlOpaqueSyclProgram *DpctlSyclProgramRef;
 
 /*!
  * @brief Opaque pointer to a ``sycl::queue``
  *
  */
-typedef struct DPCTLOpaqueSyclQueue *DPCTLSyclQueueRef;
+typedef struct DpctlOpaqueSyclQueue *DpctlSyclQueueRef;
 
 /*!
- * @brief Used to pass a ``sycl::usm`` memory opaquely through DPCTL interfaces.
+ * @brief Used to pass a ``sycl::usm`` memory opaquely through Dpctl interfaces.
  *
  */
-typedef struct DPCTLOpaqueSyclUSM *DPCTLSyclUSMRef;
+typedef struct DpctlOpaqueSyclUSM *DpctlSyclUSMRef;
 
 DPCTL_C_EXTERN_C_END
