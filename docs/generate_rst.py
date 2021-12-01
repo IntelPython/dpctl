2b56f03edd0999363fdfdcdb8958319bf17148fa
Diptorup Deb <diptorup.deb@intel.com>
2021-12-01 16:30:23 -0600

    Add a function to generate a summary page for each module.

--- docs/generate_rst.py
+++ docs/generate_rst.py
@@ -23,6 +23,8 @@ import inspect
 import io
 import os
 import sys
+from importlib import import_module
+from pkgutil import iter_modules
 
 import dpctl
 
@@ -32,6 +34,27 @@ _getset_descriptor = type(dpctl.SyclDevice.name)
 _cython_method_type = type(dpctl.SyclDevice.get_filter_string)
 # known builtin method (defined using cpdef in Cython extension class)
 _cython_builtin_function_or_method_type = type(dpctl.SyclQueue.mro)
+
+
+def _write_line(output, s):
+    output.write(s)
+    output.write("\n")
+
+
+def _write_empty_line(output):
+    _write_line(output, "")
+
+
+def _write_marquee(o, s):
+    marquee = "#" * len(s)
+    _write_line(o, marquee)
+    _write_line(o, s)
+    _write_line(o, marquee)
+
+
+def _write_underlined(o, s, c):
+    _write_line(o, s)
+    _write_line(o, c * len(s))
 
 
 def _get_public_class_name(cls):
@@ -84,35 +107,25 @@ def generate_class_rst(cls):
     rst_header = cls_qualname.split(".")[-1]
     rst_module = ".".join(cls_qualname.split(".")[:-1])
     rst_header = "".join([".. _", rst_header, "_api:"])
-    empty_line = ""
-
-    def write_line(o, s):
-        o.write(s)
-        o.write("\n")
 
     def write_rubric(o, indent, rubric_display, rubric_tag, cls_qualname):
-        write_line(o, indent + ".. rubric:: " + rubric_display)
-        write_line(o, "")
-        write_line(o, indent + ".. autosummary:: " + cls_qualname)
-        write_line(o, indent + indent + ":" + rubric_tag + ":")
-        write_line(o, "")
-
-    def write_underlined(o, s, c):
-        write_line(o, s)
-        write_line(o, c * len(s))
+        _write_line(o, indent + ".. rubric:: " + rubric_display)
+        _write_empty_line(o)
+        _write_line(o, indent + ".. autoautosummary:: " + cls_qualname)
+        _write_line(o, indent + indent + ":" + rubric_tag + ":")
+        _write_empty_line(o)
 
     with io.StringIO() as output:
-        write_line(output, rst_header)
-        write_line(output, empty_line)
-        marquee = "#" * len(cls_qualname)
-        write_line(output, marquee)
-        write_line(output, empty_line)
+        _write_line(output, rst_header)
+        _write_empty_line(output)
+        _write_marquee(output, cls_qualname)
+        _write_empty_line(output)
 
-        write_line(output, ".. currentmodule:: " + rst_module)
-        write_line(output, empty_line)
+        _write_line(output, ".. currentmodule:: " + rst_module)
+        _write_empty_line(output)
 
-        write_line(output, ".. autoclass:: " + cls_qualname)
-        write_line(output, empty_line)
+        _write_line(output, ".. autoclass:: " + cls_qualname)
+        _write_empty_line(output)
 
         indent = "    "
 
@@ -133,20 +146,20 @@ def generate_class_rst(cls):
             cls_qualname,
         )
 
-        write_underlined(output, "Detail", "=")
-        write_line(output, empty_line)
+        _write_underlined(output, "Detail", "=")
+        _write_empty_line(output)
 
         # Attributes
         all_attributes = _get_filtered_names(cls, _is_class_property)
         if all_attributes:
-            write_underlined(output, attributes_header, "-")
-            write_line(output, empty_line)
+            _write_underlined(output, attributes_header, "-")
+            _write_empty_line(output)
             for n in all_attributes:
-                write_line(
+                _write_line(
                     output,
                     ".. autoattribute:: " + ".".join([cls_qualname, n]),
                 )
-            write_line(output, empty_line)
+            _write_empty_line(output)
 
         # Methods, separated into public/private
         all_methods = _get_filtered_names(cls, _is_class_method)
@@ -159,26 +172,132 @@ def generate_class_rst(cls):
                 all_private_methods.append(_name)
 
         if all_public_methods:
-            write_underlined(output, public_methods_header, "-")
-            write_line(output, empty_line)
+            _write_underlined(output, public_methods_header, "-")
+            _write_empty_line(output)
             for n in all_public_methods:
-                write_line(
+                _write_line(
                     output,
                     ".. autoattribute:: " + ".".join([cls_qualname, n]),
                 )
-            write_line(output, empty_line)
+            _write_empty_line(output)
 
         # Private methods
         if all_private_methods:
-            write_underlined(output, private_methods_header, "-")
-            write_line(output, empty_line)
+            _write_underlined(output, private_methods_header, "-")
+            _write_empty_line(output)
             for n in all_private_methods:
-                write_line(
+                _write_line(
                     output,
                     ".. autoattribute:: " + ".".join([cls_qualname, n]),
                 )
-            write_line(output, empty_line)
 
+        return output.getvalue()
+
+
+def generate_landing_rst(module):
+    """[summary]
+
+    Args:
+        module ([str]): [description]
+
+    Returns:
+        [type]: [description]
+    """
+    rst_header = "".join([".. _", module, "_pyapi:"])
+    pagename = module + " API"
+    indent = "    "
+
+    def _write_table_header(o):
+        _write_line(o, ".. list-table::")
+        _write_line(o, indent + ":widths: 25,50")
+        _write_empty_line(o)
+
+    def _write_submodule_table(o, mod):
+        _write_table_header(o)
+        for submod in iter_modules(mod.__path__):
+            if submod.ispkg:
+                _write_line(
+                    o,
+                    indent
+                    + "* - :ref:`"
+                    + module
+                    + "."
+                    + submod.name
+                    + "_api`",
+                )
+                _submod = import_module(
+                    module + "." + submod.name, mod.__name__
+                )
+                mod_summary = (
+                    ""
+                    if not _submod.__doc__
+                    else _submod.__doc__.split("\n")[0]
+                )
+                _write_line(o, indent + "  - " + mod_summary)
+        _write_empty_line(o)
+
+    def _write_classes_table(o, mod):
+        _write_table_header(o)
+        for name, obj in inspect.getmembers(mod):
+            if inspect.isclass(obj) and not (
+                issubclass(obj, enum.Enum) or issubclass(obj, Exception)
+            ):
+                _write_line(o, indent + "* - :ref:`" + name + "_api`")
+                # For classes, the first line of the docstring is the
+                # signature. So we skip that line to pick up the summary.
+                cls_summary = obj.__doc__.split("\n")[1]
+                _write_line(o, indent + "  - " + cls_summary)
+        _write_empty_line(o)
+
+    def _write_enum_table(o, mod):
+        _write_table_header(o)
+        for name, obj in inspect.getmembers(mod):
+            if inspect.isclass(obj) and issubclass(obj, enum.Enum):
+                # FIXME link into the page pointing to the actual doc
+                # section for the enum.
+                _write_line(o, indent + "* - :ref:`" + module + "_enum_api`")
+                enum_summary = obj.__doc__.split("\n")[0]
+                _write_line(o, indent + "  - " + enum_summary)
+        _write_empty_line(o)
+
+    def _write_exception_table(o, mod):
+        _write_table_header(o)
+        for name, obj in inspect.getmembers(mod):
+            if inspect.isclass(obj) and issubclass(obj, Exception):
+                _write_line(
+                    o, indent + "* - :ref:`" + module + "_exception_api`"
+                )
+                # For classes, the first line of the docstring is the
+                # signature. So we skip that line to pick up the summary.
+                excp_summary = obj.__doc__.split("\n")[1]
+                _write_line(o, indent + "  - " + excp_summary)
+        _write_empty_line(o)
+
+    mod = None
+    try:
+        mod = sys.modules[module]
+    except KeyError:
+        raise ValueError(
+            module + "is not a valid module name or it is not loaded"
+        )
+
+    with io.StringIO() as output:
+        _write_line(output, rst_header)
+        _write_empty_line(output)
+        _write_marquee(output, pagename)
+        _write_empty_line(output)
+        _write_line(output, ".. currentmodule:: " + module)
+        _write_empty_line(output)
+        _write_line(output, ".. automodule:: " + module)
+        _write_empty_line(output)
+        _write_underlined(output, "Sub-modules", "-")
+        _write_empty_line(output)
+        _write_submodule_table(output, mod)
+        _write_underlined(output, "Classes", "-")
+        _write_empty_line(output)
+        _write_classes_table(output, mod)
+        _write_enum_table(output, mod)
+        _write_exception_table(output, mod)
         return output.getvalue()
 
 
@@ -198,7 +317,7 @@ def generate_rst_for_all_classes(module, outputpath):
             module + "is not a valid module name or it is not loaded"
         )
 
-    if not os.path.lexists(outputpath):
+    if not os.path.exists(outputpath):
         raise ValueError("Invalid output path provided")
     for name, obj in inspect.getmembers(mod):
         if inspect.isclass(obj) and not issubclass(obj, enum.Enum):
