d6618d6cd47d8550083bcb2871aadecba952937a
Oleksandr Pavlyk <oleksandr.pavlyk@intel.com>
2022-01-26 09:46:52 -0600

    Make the build work on Windows

--- conda-recipe/bld.bat
+++ conda-recipe/bld.bat
@@ -4,16 +4,17 @@ set "LIB=%BUILD_PREFIX%\Library\lib;%BUILD_PREFIX%\compiler\lib;%LIB%"
 set "INCLUDE=%BUILD_PREFIX%\include;%INCLUDE%"
 
 "%PYTHON%" setup.py clean --all
-set "INSTALL_CMD=install -- -G Ninja -DDPCTL_DPCPP_HOME_DIR=%BUILD_PREFIX%\Library -DCMAKE_C_COMPILER:PATH=icx -DCMAKE_CXX_COMPILER:PATH=icx -DDPCTL_ENABLE_LO_PROGRAM_CREATION=ON"
+set "SKBUILD_ARGS=-- -G Ninja -DDPCTL_DPCPP_HOME_DIR=%BUILD_PREFIX%\Library -DCMAKE_C_COMPILER:PATH=icx -DCMAKE_CXX_COMPILER:PATH=icx -DDPCTL_ENABLE_LO_PROGRAM_CREATION=ON"
+set "SYCL_INCLUDE_DIR_HINT=%BUILD_PREFIX%\Library\lib\clang\14.0.0"
 
 if NOT "%WHEELS_OUTPUT_FOLDER%"=="" (
     rem Install and assemble wheel package from the build bits
-    "%PYTHON%" setup.py %INSTALL_CMD% bdist_wheel
+    "%PYTHON%" setup.py install bdist_wheel %SKBUILD_ARGS%
     if errorlevel 1 exit 1
     copy dist\dpctl*.whl %WHEELS_OUTPUT_FOLDER%
     if errorlevel 1 exit 1
 ) ELSE (
     rem Only install
-    "%PYTHON%" setup.py %INSTALL_CMD%
+    "%PYTHON%" setup.py install %SKBUILD_ARGS%
     if errorlevel 1 exit 1
 )
--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -7,8 +7,8 @@ export LDFLAGS="$LDFLAGS -Wl,-rpath,$PREFIX/lib"
 
 ${PYTHON} setup.py clean --all
 export CMAKE_GENERATOR="Unix Makefiles"
-INSTALL_CMD="install -- -DCMAKE_C_COMPILER:PATH=icx -DCMAKE_CXX_COMPILER:PATH=icpx -DDPCTL_ENABLE_LO_PROGRAM_CREATION=ON -DDPCTL_DPCPP_HOME_DIR=${BUILD_PREFIX}"
-echo "${PYTHON} setup.py ${INSTALL_CMD}"
+SKBUILD_ARGS="-- -DCMAKE_C_COMPILER:PATH=icx -DCMAKE_CXX_COMPILER:PATH=icpx -DDPCTL_ENABLE_LO_PROGRAM_CREATION=ON -DDPCTL_DPCPP_HOME_DIR=${BUILD_PREFIX}"
+echo "${PYTHON} setup.py install ${SKBUILD_ARGS}"
 
 # Workaround for:
 # DPC++ launched by cmake does not see components of `dpcpp_cpp_rt`,
@@ -22,9 +22,9 @@ if [ -n "${WHEELS_OUTPUT_FOLDER}" ]; then
     else
         WHEELS_BUILD_ARGS="-p manylinux2014_x86_64"
     fi
-    ${PYTHON} setup.py ${INSTALL_CMD} bdist_wheel ${WHEELS_BUILD_ARGS}
+    ${PYTHON} setup.py install bdist_wheel ${WHEELS_BUILD_ARGS} ${SKBUILD_ARGS}
     cp dist/dpctl*.whl ${WHEELS_OUTPUT_FOLDER}
 else
     # Perform regular install
-    ${PYTHON} setup.py ${INSTALL_CMD}
+    ${PYTHON} setup.py install ${SKBUILD_ARGS}
 fi
