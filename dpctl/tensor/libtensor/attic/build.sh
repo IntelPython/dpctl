03dbe1e9f706dfdcb52ce82c74c63258cbd12806
Oleksandr Pavlyk <oleksandr.pavlyk@intel.com>
2022-02-21 15:33:30 -0600

    Deleting extraneous files

--- dpctl/tensor/libtensor/attic/build.sh
+++
@@ -1,3 +0,0 @@
-g++ -Wall -shared -fPIC $(python3 -m pybind11 --includes) -I../include -L. -ltensor -Wl,-rpath,`pwd` ../attic/strided_utils.cpp -o strided_utils$(python3-config --extension-suffix)
-
-g++ ../attic/a.cpp -L. -ltensor -Wl,-rpath,`pwd`
