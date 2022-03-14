97c4d20a2a1db2d9b94c11f074d304b904da8189
Oleksandr Pavlyk <oleksandr.pavlyk@intel.com>
2022-03-14 13:53:42 -0500

    Tweaks to run_test scripts to print lsplatform and version

--- conda-recipe/run_test.bat
+++ conda-recipe/run_test.bat
@@ -1,6 +1,9 @@
 @echo on
 
-"%PYTHON%" -c "import dpctl"
+"%PYTHON%" -c "import dpctl; print(dpctl.__version__)"
+if errorlevel 1 exit 1
+
+"%PYTHON%" -c "import dpctl; dpctl.lsplatform()"
 if errorlevel 1 exit 1
 
 python -m pytest -q -ra --disable-warnings --pyargs dpctl -vv
