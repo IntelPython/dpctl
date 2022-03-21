1c718b34eae4586b75842f8726294a6d22b4d6d4
Oleksandr Pavlyk <oleksandr.pavlyk@intel.com>
2022-03-21 10:07:51 -0500

    Disable faulthandler pytest plugin also on Linux

--- conda-recipe/run_test.sh
+++ conda-recipe/run_test.sh
@@ -4,4 +4,4 @@ set -e
 
 ${PYTHON} -c "import dpctl; print(dpctl.__version__)"
 ${PYTHON} -c "import dpctl; dpctl.lsplatform()"
-${PYTHON} -m pytest -q -ra --disable-warnings --cov dpctl --cov-report term-missing --pyargs dpctl -vv
+${PYTHON} -m pytest -q -ra --disable-warnings -p no:faulthandler --cov dpctl --cov-report term-missing --pyargs dpctl -vv
