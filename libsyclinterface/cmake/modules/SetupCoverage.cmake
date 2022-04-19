916716973e160fc885c1b84de5637c29c41a0959
Oleksandr Pavlyk <oleksandr.pavlyk@intel.com>
2022-04-19 18:35:14 -0500

    propagated DPCTL_GENERATE_COVERAGE to top-level CMakeLists.txt
    Decoupled llvm-genhtml and llvm-cov targets. Used object files in
    llvm-genhtml as it was done in llvm-cov target.

---
+++ libsyclinterface/cmake/modules/SetupCoverage.cmake
@@ -0,0 +1,22 @@
+
+function(setup_coverage_generation)
+    # check if lcov is available
+    find_package(Lcov REQUIRED)
+    # check if llvm-cov version 11 is available
+    find_package(LLVMCov 11 REQUIRED)
+    # check if llvm-profdata is available
+    find_package(LLVMProfdata REQUIRED)
+
+    string(CONCAT PROFILE_FLAGS
+        "-fprofile-instr-generate "
+        "-fcoverage-mapping "
+        "-fno-sycl-use-footer "
+        "-DDPCTL_COVERAGE "
+    )
+
+    # Add profiling flags
+    set(CMAKE_CXX_FLAGS
+        "${CMAKE_CXX_FLAGS} ${PROFILE_FLAGS}"
+        PARENT_SCOPE
+    )
+endfunction(setup_coverage_generation)
