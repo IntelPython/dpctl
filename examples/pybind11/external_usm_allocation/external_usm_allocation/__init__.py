5822704c0574c9dd29e10b8277997fd6723f80b4
Oleksandr Pavlyk <oleksandr.pavlyk@intel.com>
2022-04-25 13:23:26 -0500

    Added license headers

--- examples/pybind11/external_usm_allocation/external_usm_allocation/__init__.py
+++ examples/pybind11/external_usm_allocation/external_usm_allocation/__init__.py
@@ -1,3 +1,27 @@
+#                      Data Parallel Control (dpctl)
+#
+# Copyright 2020-2021 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# coding: utf-8
+
 from ._external_usm_alloc import DMatrix
 
 __all__ = ["DMatrix"]
+
+__doc__ = """
+   Example of implementing C++ class with its own USM memory allocation logic
+and interfacing that allocation with `dpctl` by implementing
+`__sycl_usm_array_interface__`.
+"""
--- examples/pybind11/use_dpctl_syclqueue/use_queue_device/__init__.py
+++ examples/pybind11/use_dpctl_syclqueue/use_queue_device/__init__.py
@@ -1,3 +1,21 @@
+#                      Data Parallel Control (dpctl)
+#
+# Copyright 2020-2021 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# coding: utf-8
+
 from ._use_queue_device import (
     get_device_global_mem_size,
     get_device_local_mem_size,
@@ -11,3 +29,14 @@ __all__ = [
     "get_device_local_mem_size",
     "offloaded_array_mod",
 ]
+
+__doc__ = """
+Example pybind11 extension demonstrating binding of dpctl entities to
+SYCL entities.
+
+dpctl provides type casters that bind ``sycl::queue`` to `dpctl.SyclQueue`,
+``sycl::device`` to `dpctl.SyclDevice`, etc.
+
+Use of these type casters simplifies writing of Python extensions and compile
+then using SYCL C++ compilers, such as Intel(R) oneAPI DPC++ compiler.
+"""
