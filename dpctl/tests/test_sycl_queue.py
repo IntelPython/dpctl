9a3a2d0010784adb4f179349ba37f258ab62f430
Oleksandr Pavlyk <oleksandr.pavlyk@intel.com>
2022-08-06 23:20:02 -0500

    Added max_work_item_sizes1d, 2d, 3d properties
    dpctl.SyclDevice.max_work_iterm_sizes is deprecated.

--- dpctl/tests/test_sycl_queue.py
+++ dpctl/tests/test_sycl_queue.py
@@ -68,8 +68,20 @@ def check_get_max_work_item_dims(device):
     assert max_work_item_dims > 0
 
 
-def check_get_max_work_item_sizes(device):
-    max_work_item_sizes = device.max_work_item_sizes
+def check_get_max_work_item_sizes1d(device):
+    max_work_item_sizes = device.max_work_item_sizes1d
+    for size in max_work_item_sizes:
+        assert size is not None
+
+
+def check_get_max_work_item_sizes2d(device):
+    max_work_item_sizes = device.max_work_item_sizes2d
+    for size in max_work_item_sizes:
+        assert size is not None
+
+
+def check_get_max_work_item_sizes3d(device):
+    max_work_item_sizes = device.max_work_item_sizes3d
     for size in max_work_item_sizes:
         assert size is not None
 
@@ -263,7 +275,9 @@ def check_is_host(device):
 list_of_checks = [
     check_get_max_compute_units,
     check_get_max_work_item_dims,
-    check_get_max_work_item_sizes,
+    check_get_max_work_item_sizes1d,
+    check_get_max_work_item_sizes2d,
+    check_get_max_work_item_sizes3d,
     check_get_max_work_group_size,
     check_get_max_num_sub_groups,
     check_is_accelerator,
