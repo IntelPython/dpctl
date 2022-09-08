9f761dde3eff61e6eacf03770c239b6261f78df8
Oleksandr Pavlyk <oleksandr.pavlyk@intel.com>
2022-09-08 15:28:55 -0500

    Fixed copyright year range

--- libsyclinterface/tests/test_sycl_device_aspects.cpp
+++ libsyclinterface/tests/test_sycl_device_aspects.cpp
@@ -1,3 +1,29 @@
+//===--- test_sycl_device_aspects.cpp - Test cases for device interface ===//
+//
+//                      Data Parallel Control (dpctl)
+//
+// Copyright 2020-2022 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// This file has unit test cases for functions defined in
+/// dpctl_sycl_device_interface.h concernings device aspects.
+///
+//===----------------------------------------------------------------------===//
+
 #include "Support/CBindingWrapping.h"
 #include "dpctl_sycl_device_interface.h"
 #include "dpctl_sycl_device_selector_interface.h"
