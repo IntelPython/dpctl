95a8142e5aa8b490686565afcac4b940ee40faf0
Oleksandr Pavlyk <oleksandr.pavlyk@intel.com>
2023-05-16 11:11:19 -0500

    Use verbose lsplatform in run_test.sh

--- conda-recipe/run_test.sh
+++ conda-recipe/run_test.sh
@@ -3,5 +3,5 @@
 set -e
 
 ${PYTHON} -c "import dpctl; print(dpctl.__version__)"
-${PYTHON} -c "import dpctl; dpctl.lsplatform()"
+${PYTHON} -c "import dpctl; dpctl.lsplatform(verbosity=2)"
 ${PYTHON} -m pytest -q -ra --disable-warnings -p no:faulthandler --cov dpctl --cov-report term-missing --pyargs dpctl -vv
