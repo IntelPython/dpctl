00a33b03a938b0ab93ec317f1ef35277ca328234
Nikita Grigorian <nikita.grigorian@intel.com>
2023-06-22 13:59:07 -0700

    sycl::vec overload for sine

--- dpctl/tensor/libtensor/include/kernels/elementwise_functions/sin.hpp
+++ dpctl/tensor/libtensor/include/kernels/elementwise_functions/sin.hpp
@@ -58,7 +58,8 @@ template <typename argT, typename resT> struct SinFunctor
     // constant value, if constant
     // constexpr resT constant_value = resT{};
     // is function defined for sycl::vec
-    using supports_vec = typename std::false_type;
+    using supports_vec = typename std::negation<
+        std::disjunction<is_complex<resT>, is_complex<argT>>>;
     // do both argTy and resTy support sugroup store/load operation
     using supports_sg_loadstore = typename std::negation<
         std::disjunction<is_complex<resT>, is_complex<argT>>>;
@@ -179,6 +180,20 @@ template <typename argT, typename resT> struct SinFunctor
             static_assert(std::is_floating_point_v<argT> ||
                           std::is_same_v<argT, sycl::half>);
             return std::sin(in);
+        }
+    }
+
+    template <int vec_sz>
+    sycl::vec<resT, vec_sz> operator()(const sycl::vec<argT, vec_sz> &in)
+    {
+        auto const &res_vec = sycl::sin(in);
+        using deducedT = typename std::remove_cv_t<
+            std::remove_reference_t<decltype(res_vec)>>::element_type;
+        if constexpr (std::is_same_v<resT, deducedT>) {
+            return res_vec;
+        }
+        else {
+            return vec_cast<resT, deducedT, vec_sz>(res_vec);
         }
     }
 };
