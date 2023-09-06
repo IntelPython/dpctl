03e7f264f403b94479f10a9d50bd7826580cca3d
Oleksandr Pavlyk <oleksandr.pavlyk@intel.com>
2023-09-06 00:52:21 -0500

    address_space_cast<address_space::private_space,...> on local variable
    To create the multi_ptr from a local variable (in private memory) we should be
    using address_space::private_space.

--- dpctl/tensor/libtensor/include/kernels/elementwise_functions/expm1.hpp
+++ dpctl/tensor/libtensor/include/kernels/elementwise_functions/expm1.hpp
@@ -116,7 +116,7 @@ template <typename argT, typename resT> struct Expm1Functor
             // x, y finite numbers
             realT cosY_val;
             auto cosY_val_multi_ptr = sycl::address_space_cast<
-                sycl::access::address_space::global_space,
+                sycl::access::address_space::private_space,
                 sycl::access::decorated::yes>(&cosY_val);
             const realT sinY_val = sycl::sincos(y, cosY_val_multi_ptr);
             const realT sinhalfY_val = std::sin(y / 2);
