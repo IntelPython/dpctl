d58d58fd69b853f54d7701022f3a793a3620b385
Oleksandr Pavlyk <oleksandr.pavlyk@intel.com>
2023-09-20 14:55:24 -0500

    Skip testing complex special values on Windows

--- dpctl/tests/elementwise/test_trigonometric.py
+++ dpctl/tests/elementwise/test_trigonometric.py
@@ -15,6 +15,7 @@
 #  limitations under the License.
 
 import itertools
+import os
 
 import numpy as np
 import pytest
@@ -267,6 +268,7 @@ def test_trig_real_special_cases(np_call, dpt_call, dtype):
     assert_allclose(dpt.asnumpy(dpt_call(yf)), Y_np, atol=tol, rtol=tol)
 
 
+@pytest.mark.skipif(os.name == "nt", reason="Known problem on Windows")
 @pytest.mark.parametrize("np_call, dpt_call", _all_funcs)
 @pytest.mark.parametrize("dtype", ["c8", "c16"])
 def test_trig_complex_special_cases(np_call, dpt_call, dtype):
