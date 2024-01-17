3ce9b59dd29faaca1d15c6376f39f15ee7943d65
Oleksandr Pavlyk <oleksandr.pavlyk@intel.com>
2024-01-17 12:54:17 -0600

    Use result_type with tensors to take device capability into account

--- dpctl/tests/test_usm_ndarray_linalg.py
+++ dpctl/tests/test_usm_ndarray_linalg.py
@@ -656,7 +656,7 @@ def test_tensordot_promotion():
 
     t3 = dpt.zeros((10, 10), dtype="u4")
     r3 = dpt.tensordot(t1, t3)
-    assert r3.dtype == dpt.result_type(t1.dtype, t3.dtype)
+    assert r3.dtype == dpt.result_type(t1, t3)
 
 
 def test_tensordot_axes_errors():
