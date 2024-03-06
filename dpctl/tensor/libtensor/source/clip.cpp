074ab3d507b63ab69ef4c712361dce49c12ce158
Oleksandr Pavlyk <oleksandr.pavlyk@intel.com>
2024-03-05 19:19:47 -0600

    Handle allocation error for USM-host for strides

--- dpctl/tensor/libtensor/source/clip.cpp
+++ dpctl/tensor/libtensor/source/clip.cpp
@@ -225,6 +225,9 @@ py_clip(const dpctl::tensor::usm_ndarray &src,
         simplified_shape, simplified_src_strides, simplified_min_strides,
         simplified_max_strides, simplified_dst_strides);
     py::ssize_t *packed_shape_strides = std::get<0>(ptr_size_event_tuple);
+    if (!packed_shape_strides) {
+        throw std::runtime_error("USM-host memory allocation failure");
+    }
     sycl::event copy_shape_strides_ev = std::get<2>(ptr_size_event_tuple);
 
     std::vector<sycl::event> all_deps;
