cf9401772f2ed156bc14847813d3f95f655136dc
Oleksandr Pavlyk <oleksandr.pavlyk@intel.com>
2024-03-11 04:51:36 -0500

    Change script to use llvm-cov-report instead of llvm-cov

--- scripts/gen_coverage.py
+++ scripts/gen_coverage.py
@@ -82,7 +82,7 @@ def run(
         .strip("\n")
     )
     subprocess.check_call(
-        ["cmake", "--build", ".", "--target", "llvm-cov"],
+        ["cmake", "--build", ".", "--target", "llvm-cov-report"],
         cwd=cmake_build_dir,
     )
     env["LLVM_PROFILE_FILE"] = "dpctl_pytest.profraw"
