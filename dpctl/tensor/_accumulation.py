015079b85fda3ff704b76ebf645ab5e6f0f18a30
Nikita Grigorian <nikita.grigorian@intel.com>
2024-03-25 21:43:52 -0700

    Implements `out` keyword for accumulators

--- dpctl/tensor/_accumulation.py
+++ dpctl/tensor/_accumulation.py
@@ -23,6 +23,7 @@ import dpctl.tensor as dpt
 import dpctl.tensor._tensor_accumulation_impl as tai
 import dpctl.tensor._tensor_impl as ti
 from dpctl.tensor._type_utils import _to_device_supported_dtype
+from dpctl.utils import ExecutionPlacementError
 
 
 def _default_accumulation_dtype(inp_dt, q):
@@ -75,6 +76,7 @@ def _accumulate_over_axis(
     axis,
     dtype,
     include_initial,
+    out,
     _accumulate_fn,
     _accumulate_include_initial_fn,
     _dtype_supported,
@@ -83,13 +85,18 @@ def _accumulate_over_axis(
     if not isinstance(x, dpt.usm_ndarray):
         raise TypeError(f"Expected dpctl.tensor.usm_ndarray, got {type(x)}")
     nd = x.ndim
+    sh = x.shape
     if axis is None:
         if nd > 1:
-            raise ValueError
+            raise ValueError(
+                "`axis` cannot be `None` for array of dimension `{}`".format(nd)
+            )
         axis = 0
     else:
-        axis = operator.index(axis)
-    axis = normalize_axis_index(axis, nd, "axis")
+        axis = normalize_axis_index(operator.index(axis), nd, "axis")
+    res_sh = (
+        sh[:axis] + (sh[axis] + 1,) + sh[axis + 1 :] if include_initial else sh
+    )
     a1 = axis + 1
     if a1 == nd:
         perm = list(range(nd))
@@ -101,40 +108,77 @@ def _accumulate_over_axis(
         arr = dpt.permute_dims(x, perm)
     q = x.sycl_queue
     inp_dt = x.dtype
+    res_usm_type = x.usm_type
     if dtype is None:
         res_dt = _default_accumulation_type_fn(inp_dt, q)
     else:
         res_dt = dpt.dtype(dtype)
         res_dt = _to_device_supported_dtype(res_dt, q.sycl_device)
-    sh = arr.shape
-    res_sh = sh[:-1] + (sh[-1] + 1,) if include_initial else sh
-    res_usm_type = x.usm_type
+
+    # checking now avoids unnecessary allocations
+    implemented_types = _dtype_supported(inp_dt, res_dt)
+    if dtype is None and not implemented_types:
+        raise RuntimeError(
+            "Automatically determined accumulation data type does not "
+            "have direct implementation"
+        )
+    orig_out = out
+    if out is not None:
+        if not isinstance(out, dpt.usm_ndarray):
+            raise TypeError(
+                f"output array must be of usm_ndarray type, got {type(out)}"
+            )
+        if not out.flags.writable:
+            raise ValueError("provided `out` array is read-only")
+        if out.shape != res_sh:
+            raise ValueError(
+                "The shape of input and output arrays are inconsistent. "
+                f"Expected output shape is {res_sh}, got {out.shape}"
+            )
+        if res_dt != out.dtype:
+            raise ValueError(
+                f"Output array of type {res_dt} is needed, " f"got {out.dtype}"
+            )
+        if dpctl.utils.get_execution_queue((q, out.sycl_queue)) is None:
+            raise ExecutionPlacementError(
+                "Input and output allocation queues are not compatible"
+            )
+        # permute out array dims if necessary
+        if a1 != nd:
+            out = dpt.permute_dims(out, perm)
+            orig_out = out
+        if ti._array_overlap(x, out) and implemented_types:
+            out = dpt.empty_like(out)
 
     host_tasks_list = []
-    if _dtype_supported(inp_dt, res_dt):
-        res = dpt.empty(
-            res_sh, dtype=res_dt, usm_type=res_usm_type, sycl_queue=q
-        )
+    if implemented_types:
+        if out is None:
+            out = dpt.empty(
+                res_sh, dtype=res_dt, usm_type=res_usm_type, sycl_queue=q
+            )
+            out = dpt.permute_dims(out, perm)
         if not include_initial:
-            ht_e, _ = _accumulate_fn(
+            ht_e, acc_ev = _accumulate_fn(
                 src=arr,
                 trailing_dims_to_accumulate=1,
-                dst=res,
+                dst=out,
                 sycl_queue=q,
             )
         else:
-            ht_e, _ = _accumulate_include_initial_fn(
+            ht_e, acc_ev = _accumulate_include_initial_fn(
                 src=arr,
-                dst=res,
+                dst=out,
                 sycl_queue=q,
             )
         host_tasks_list.append(ht_e)
-    else:
-        if dtype is None:
-            raise RuntimeError(
-                "Automatically determined accumulation data type does not "
-                "have direct implementation"
+        if not (orig_out is None or out is orig_out):
+            # Copy the out data from temporary buffer to original memory
+            ht_e_cpy, _ = ti._copy_usm_ndarray_into_usm_ndarray(
+                src=out, dst=orig_out, sycl_queue=q, depends=[acc_ev]
             )
+            host_tasks_list.append(ht_e_cpy)
+            out = orig_out
+    else:
         if _dtype_supported(res_dt, res_dt):
             tmp = dpt.empty(
                 arr.shape, dtype=res_dt, usm_type=res_usm_type, sycl_queue=q
@@ -143,25 +187,34 @@ def _accumulate_over_axis(
                 src=arr, dst=tmp, sycl_queue=q
             )
             host_tasks_list.append(ht_e_cpy)
-            res = dpt.empty(
-                res_sh, dtype=res_dt, usm_type=res_usm_type, sycl_queue=q
-            )
+            if out is None:
+                out = dpt.empty(
+                    res_sh, dtype=res_dt, usm_type=res_usm_type, sycl_queue=q
+                )
+                out = dpt.permute_dims(out, perm)
             if not include_initial:
-                ht_e, _ = _accumulate_fn(
+                ht_e, acc_ev = _accumulate_fn(
                     src=tmp,
                     trailing_dims_to_accumulate=1,
-                    dst=res,
+                    dst=out,
                     sycl_queue=q,
                     depends=[cpy_e],
                 )
             else:
-                ht_e, _ = _accumulate_include_initial_fn(
+                ht_e, acc_ev = _accumulate_include_initial_fn(
                     src=tmp,
-                    dst=res,
+                    dst=out,
                     sycl_queue=q,
                     depends=[cpy_e],
                 )
             host_tasks_list.append(ht_e)
+            if not (orig_out is None or out is orig_out):
+                # Copy the out data from temporary buffer to original memory
+                ht_e_cpy2, _ = ti._copy_usm_ndarray_into_usm_ndarray(
+                    src=out, dst=orig_out, sycl_queue=q, depends=[acc_ev]
+                )
+                host_tasks_list.append(ht_e_cpy2)
+                out = orig_out
         else:
             buf_dt = _default_accumulation_dtype(inp_dt, q)
             tmp = dpt.empty(
@@ -174,9 +227,11 @@ def _accumulate_over_axis(
                 res_sh, dtype=buf_dt, usm_type=res_usm_type, sycl_queue=q
             )
             host_tasks_list.append(ht_e_cpy)
-            res = dpt.empty(
-                res_sh, dtype=res_dt, usm_type=res_usm_type, sycl_queue=q
-            )
+            if out is None:
+                out = dpt.empty(
+                    res_sh, dtype=res_dt, usm_type=res_usm_type, sycl_queue=q
+                )
+                out = dpt.permute_dims(out, perm)
             if not include_initial:
                 ht_e, a_e = _accumulate_fn(
                     src=arr,
@@ -193,24 +248,35 @@ def _accumulate_over_axis(
                     depends=[cpy_e],
                 )
             host_tasks_list.append(ht_e)
-            ht_e_cpy2, _ = ti._copy_usm_ndarray_into_usm_ndarray(
-                src=tmp_res, dst=res, sycl_queue=q, depends=[a_e]
+            ht_e_cpy2, cpy_e2 = ti._copy_usm_ndarray_into_usm_ndarray(
+                src=tmp_res, dst=out, sycl_queue=q, depends=[a_e]
             )
             host_tasks_list.append(ht_e_cpy2)
+            if not (orig_out is None or out is orig_out):
+                # Copy the out data from temporary buffer to original memory
+                ht_e_cpy3, _ = ti._copy_usm_ndarray_into_usm_ndarray(
+                    src=out, dst=orig_out, sycl_queue=q, depends=[cpy_e2]
+                )
+                host_tasks_list.append(ht_e_cpy3)
+                out = orig_out
+
     if a1 != nd:
         inv_perm = sorted(range(nd), key=lambda d: perm[d])
-        res = dpt.permute_dims(res, inv_perm)
+        out = dpt.permute_dims(out, inv_perm)
     dpctl.SyclEvent.wait_for(host_tasks_list)
 
-    return res
+    return out
 
 
-def cumulative_sum(x, axis=None, dtype=None, include_initial=False):
+def cumulative_sum(
+    x, /, *, axis=None, dtype=None, include_initial=False, out=None
+):
     return _accumulate_over_axis(
         x,
         axis,
         dtype,
         include_initial,
+        out,
         tai._cumsum_over_axis,
         tai._cumsum_final_axis_include_initial,
         tai._cumsum_dtype_supported,
@@ -218,12 +284,15 @@ def cumulative_sum(x, axis=None, dtype=None, include_initial=False):
     )
 
 
-def cumulative_prod(x, axis=None, dtype=None, include_initial=False):
+def cumulative_prod(
+    x, /, *, axis=None, dtype=None, include_initial=False, out=None
+):
     return _accumulate_over_axis(
         x,
         axis,
         dtype,
         include_initial,
+        out,
         tai._cumprod_over_axis,
         tai._cumprod_final_axis_include_initial,
         tai._cumprod_dtype_supported,
@@ -231,12 +300,15 @@ def cumulative_prod(x, axis=None, dtype=None, include_initial=False):
     )
 
 
-def cumulative_logsumexp(x, axis=None, dtype=None, include_initial=False):
+def cumulative_logsumexp(
+    x, /, *, axis=None, dtype=None, include_initial=False, out=None
+):
     return _accumulate_over_axis(
         x,
         axis,
         dtype,
         include_initial,
+        out,
         tai._cumlogsumexp_over_axis,
         tai._cumlogsumexp_final_axis_include_initial,
         tai._cumlogsumexp_dtype_supported,
