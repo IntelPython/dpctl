eaa4011fea74379ba4655da65217cd5a24bada83
Nikita Grigorian <nikita.grigorian@intel.com>
2024-03-26 11:15:58 -0700

    Adds test for scalar inputs to cumulative_sum

--- dpctl/tests/test_tensor_accumulation.py
+++ dpctl/tests/test_tensor_accumulation.py
@@ -1,4 +1,5 @@
 import pytest
+from helper import get_queue_or_skip
 
 import dpctl.tensor as dpt
 
@@ -24,9 +25,12 @@ cfp_types = [
     dpt.complex128,
 ]
 
+all_types = [dpt.bool] + sint_types + uint_types + rfp_types + cfp_types
 
-@pytest.mark.parametrize("dt", sint_types[2:])
+
+@pytest.mark.parametrize("dt", sint_types)
 def test_contig_cumsum_sint(dt):
+    get_queue_or_skip()
     n = 10000
     x = dpt.repeat(dpt.asarray([1, -1], dtype=dt), n)
 
@@ -37,8 +41,9 @@ def test_contig_cumsum_sint(dt):
     assert dpt.all(res == expected)
 
 
-@pytest.mark.parametrize("dt", sint_types[2:])
+@pytest.mark.parametrize("dt", sint_types)
 def test_strided_cumsum_sint(dt):
+    get_queue_or_skip()
     n = 10000
     x = dpt.repeat(dpt.asarray([1, -1], dtype=dt), 2 * n)[1::2]
 
@@ -57,8 +62,9 @@ def test_strided_cumsum_sint(dt):
     assert dpt.all(res == expected)
 
 
-@pytest.mark.parametrize("dt", sint_types[2:])
+@pytest.mark.parametrize("dt", sint_types)
 def test_contig_cumsum_axis_sint(dt):
+    get_queue_or_skip()
     n0, n1 = 1000, 173
     x = dpt.repeat(dpt.asarray([1, -1], dtype=dt), n0)
     m = dpt.tile(dpt.expand_dims(x, axis=1), (1, n1))
@@ -70,8 +76,9 @@ def test_contig_cumsum_axis_sint(dt):
     assert dpt.all(res == dpt.expand_dims(expected, axis=1))
 
 
-@pytest.mark.parametrize("dt", sint_types[2:])
+@pytest.mark.parametrize("dt", sint_types)
 def test_strided_cumsum_axis_sint(dt):
+    get_queue_or_skip()
     n0, n1 = 1000, 173
     x = dpt.repeat(dpt.asarray([1, -1], dtype=dt), 2 * n0)
     m = dpt.tile(dpt.expand_dims(x, axis=1), (1, n1))[1::2, ::-1]
@@ -81,3 +88,16 @@ def test_strided_cumsum_axis_sint(dt):
     ar = dpt.arange(n0, dtype=dt)
     expected = dpt.concat((1 + ar, dpt.flip(ar)))
     assert dpt.all(res == dpt.expand_dims(expected, axis=1))
+
+
+def test_accumulate_scalar():
+    get_queue_or_skip()
+
+    s = dpt.asarray(1, dtype="i8")
+    r = dpt.cumulative_sum(s)
+    assert r == s
+    assert r.ndim == s.ndim
+
+    r = dpt.cumulative_sum(s, include_initial=True)
+    r_expected = dpt.asarray([0, 1], dtype="i8")
+    assert dpt.all(r == r_expected)
