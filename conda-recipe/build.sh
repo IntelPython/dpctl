f752721ac19dd69d61237949ad475940922bf463
Oleksandr Pavlyk <oleksandr.pavlyk@intel.com>
2024-06-07 16:40:11 -0500

    Implement manipulation of flattened SO files

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -26,9 +26,31 @@ CMAKE_ARGS="${CMAKE_ARGS} -DDPCTL_LEVEL_ZERO_INCLUDE_DIR=${PREFIX}/include/level
 
 # -wnx flags mean: --wheel --no-isolation --skip-dependency-check
 ${PYTHON} -m build -w -n -x
+
+pushd dist
+${PYTHON} -m wheel unpack -d dpctl_wheel dpctl*.whl
+export lib_name=libDPCTLSyclInterface
+export so_full_path=$(find dpctl_wheel -regextype posix-extended -regex "^.*${lib_name}\.so")
+export sox_full_path=$(find dpctl_wheel -regextype posix-extended -regex "^.*${lib_name}\.so\.[0-9]*$")
+export soxxx_full_path=$(find dpctl_wheel -regextype posix-extended -regex "^.*${lib_name}\.so\.[0-9]*\.[0-9]*$")
+
+rm -rf ${so_full_path} ${soxxx_full_path}
+
+export so_name=$(basename ${so_full_path})
+export sox_name=$(basename ${sox_full_path})
+export soxxx_name=$(basename ${soxxx_full_path})
+export wheel_path=$(dirname $(dirname ${so_full_path}))
+
+# deal with hard copies
+${PYTHON} -m wheel pack ${wheel_path}
+
+rm -rf dpctl_wheel
+popd
+
 ${PYTHON} -m wheel tags --remove --build "$GIT_DESCRIBE_NUMBER" \
     --platform-tag "manylinux_${GLIBC_MAJOR}_${GLIBC_MINOR}_x86_64" \
     dist/dpctl*.whl
+
 ${PYTHON} -m pip install dist/dpctl*.whl \
     --no-build-isolation \
     --no-deps \
@@ -37,11 +59,14 @@ ${PYTHON} -m pip install dist/dpctl*.whl \
     --prefix "${PREFIX}" \
     -vv
 
+export libdir=$(find $PREFIX -name 'libDPCTLSyclInterface*' -exec dirname \{\} \;)
+
 # Recover symbolic links
 # libDPCTLSyclInterface.so.0 -> libDPCTLSyclInterface.so.0.17
 # libDPCTLSyclInterface.so -> libDPCTLSyclInterface.so.0
-find $PREFIX | grep libDPCTLSyclInterface | sort -r | \
-awk '{if ($0=="") ln=""; else if (ln=="") ln = $0; else system("rm " $0 ";\tln -s " ln " " $0); ln = $0 }'
+mv ${libdir}/${sox_name} ${libdir}/${soxxx_name}
+ln -s ${libdir}/${soxxx_name} ${libdir}/${sox_name}
+ln -s ${libdir}/${sox_name} ${libdir}/${so_name}
 
 # Copy wheel package
 if [[ -v WHEELS_OUTPUT_FOLDER ]]; then
