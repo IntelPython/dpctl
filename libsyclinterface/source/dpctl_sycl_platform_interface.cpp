226aed77e7208d7a8bce9f430f92fd52764e0f1a
Nikita Grigorian <nikita.grigorian@intel.com>
2025-03-01 01:09:06 -0800

    Add comment to DPCTLPlatform_GetDevices
    This comment describes why automatic and custom device type are handled as unknown

--- libsyclinterface/source/dpctl_sycl_platform_interface.cpp
+++ libsyclinterface/source/dpctl_sycl_platform_interface.cpp
@@ -294,6 +294,9 @@ DPCTLPlatform_GetDevices(__dpctl_keep const DPCTLSyclPlatformRef PRef,
     }
 
     // handle unknown device
+    // custom and automatic are also treated as unknown
+    // as DPC++ would normally treat as `all`
+    // see CMPLRLLVM-65826
     if (DTy == DPCTLSyclDeviceType::DPCTL_UNKNOWN_DEVICE) {
         return wrap<vecTy>(DevicesVectorPtr);
     }
