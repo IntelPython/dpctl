2146f2f190117372f685bc009244e8f024694648
Harlow, Jordan <jordan.harlow@intel.com>
2025-04-09 09:27:58 -0600

    chore: last attempt at dynamic generation

--- benchmarks/benchmarks/binary.py
+++ benchmarks/benchmarks/binary.py
@@ -1,69 +1,15 @@
 import dpctl
 import dpctl.tensor as dpt
 
+
 class Binary:
     def __init__(self):
-        self.q = dpctl.SyclQueue(property='enable_profiling')
+        self.q = dpctl.SyclQueue(property="enable_profiling")
         self.iterations = 1
-        self.zie = 2**27,
-        self.function_list = [
-            dpt.add,
-            dpt.multiply,
-            dpt.divide,
-            dpt.subtract,
-            dpt.floor_divide,
-            dpt.remainder,
-            dpt.hypot,
-            dpt.logaddexp,
-            dpt.pow,
-            dpt.atan2,
-            dpt.nextafter,
-            dpt.copysign,
-            dpt.less,
-            dpt.less_equal,
-            dpt.greater,
-            dpt.greater_equal,
-            dpt.equal,
-            dpt.not_equal,
-            dpt.minimum,
-            dpt.maximum,
-            dpt.bitwise_and,
-            dpt.bitwise_or,
-            dpt.bitwise_xor,
-            dpt.bitwise_left_shift,
-            dpt.bitwise_right_shift,
-            dpt.logical_and,
-            dpt.logical_or,
-            dpt.logical_xor
-        ]
-
-        self.dtypes = {}
-        for fn in self.function_list:
-            self.dtypes[fn] = [list(map(dpt.dtype, sig.split('->')[0])) for sig in fn.types]
-
-        # Dynamically generate benchmark functions
-        self.generate_benchmark_functions()
-
-
-    def generate_benchmark_functions(self):
-        """Dynamically create benchmark functions for each function and dtype combination."""
-        for fn in self.function_list:
-            fn_name = fn.__name__
-            for dtype1, dtype2 in self.dtypes[fn]:
-                # Create a unique function name
-                method_name = f"time_{fn_name}_{dtype1.name}_{dtype2.name}"
-
-                # Define the benchmark function
-                def benchmark_method(self, fn=fn, dtype1=dtype1, dtype2=dtype2):
-                    return self.run_bench2(self.q, self.iterations, self.n_values, dtype1, dtype2, fn)
-
-                # Set the method name and attach it to the class
-                benchmark_method.__name__ = method_name
-                setattr(self.__class__, method_name, benchmark_method)
-
+        self.n_values = 2**27
 
     def run_bench2(self, q, reps, n_max, dtype1, dtype2, op):
-
+        """Run the benchmark for a specific function and dtype combination."""
         def get_sizes(n):
             s = []
             m = 8192
@@ -77,8 +23,7 @@ class Binary:
         x2 = dpt.ones(n_max, dtype=dtype2, sycl_queue=q)
         r = op(x1, x2)
 
-        max_bytes = (x1.nbytes + x2.nbytes + r.nbytes)
-
+        max_bytes = x1.nbytes + x2.nbytes + r.nbytes
         times_res = []
 
         for n in get_sizes(n_max):
@@ -94,10 +39,82 @@ class Binary:
                 with timer(q):
                     for _ in range(n_iters):
                         op(x1_n, x2_n, out=r_n)
-                    
+
                 dev_dt = timer.dt.device_dt
                 if dev_dt > 0:
                     times_res.append((n, dev_dt / n_iters))
                     break
 
         return times_res
+
+
+# Create the SYCL queue and function list at the module level
+binary_instance = Binary()
+binary_instance.q = dpctl.SyclQueue(property="enable_profiling")
+binary_instance.iterations = 1
+binary_instance.n_values = 2**27
+
+function_list = [
+    dpt.add,
+    dpt.multiply,
+    dpt.divide,
+    dpt.subtract,
+    dpt.floor_divide,
+    dpt.remainder,
+    dpt.hypot,
+    dpt.logaddexp,
+    dpt.pow,
+    dpt.atan2,
+    dpt.nextafter,
+    dpt.copysign,
+    dpt.less,
+    dpt.less_equal,
+    dpt.greater,
+    dpt.greater_equal,
+    dpt.equal,
+    dpt.not_equal,
+    dpt.minimum,
+    dpt.maximum,
+    dpt.bitwise_and,
+    dpt.bitwise_or,
+    dpt.bitwise_xor,
+    dpt.bitwise_left_shift,
+    dpt.bitwise_right_shift,
+    dpt.logical_and,
+    dpt.logical_or,
+    dpt.logical_xor,
+]
+
+# Generate dtype combinations for each function
+dtypes = {}
+for fn in function_list:
+    dtypes[fn] = [list(map(dpt.dtype, sig.split("->")[0])) for sig in fn.types]
+
+
+# Dynamically create benchmark methods at the module level
+def generate_benchmark_functions():
+    """Dynamically create benchmark functions for each function and dtype combination."""
+    for fn in function_list:
+        fn_name = fn.__name__
+        for dtype1, dtype2 in dtypes[fn]:
+            # Create a unique function name
+            method_name = f"time_{fn_name}_{dtype1.name}_{dtype2.name}"
+
+            # Define the benchmark function
+            def benchmark_method(self, fn=fn, dtype1=dtype1, dtype2=dtype2):
+                return binary_instance.run_bench2(
+                    binary_instance.q,
+                    binary_instance.iterations,
+                    binary_instance.n_values,
+                    dtype1,
+                    dtype2,
+                    fn,
+                )
+
+            # Attach the method to the Binary class
+            benchmark_method.__name__ = method_name
+            setattr(Binary, method_name, benchmark_method)
+
+
+# Generate the benchmark functions
+generate_benchmark_functions()
