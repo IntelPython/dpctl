762aed2644075e5b51bc9db99bee10d86337815d
Harlow, Jordan <jordan.harlow@intel.com>
2025-04-09 08:21:53 -0600

    task: better configure benchmarks

--- benchmarks/benchmarks/binary.py
+++ benchmarks/benchmarks/binary.py
@@ -2,25 +2,64 @@ import dpctl
 import dpctl.tensor as dpt
 
 class Binary:
-
-    params = (
-        [dpctl.SyclQueue(property='enable_profiling')],
-        [1],
-        [2**27],
-        [
-            dpt.add, dpt.multiply, dpt.divide, dpt.subtract,
-            dpt.floor_divide, dpt.remainder,
-            dpt.hypot, dpt.logaddexp, dpt.pow, dpt.atan2, dpt.nextafter, dpt.copysign,
-            dpt.less, dpt.less_equal, dpt.greater, dpt.greater_equal, dpt.equal, dpt.not_equal,
-            dpt.minimum, dpt.maximum, dpt.bitwise_and, dpt.bitwise_or, dpt.bitwise_xor,
-            dpt.bitwise_left_shift, dpt.bitwise_right_shift, dpt.logical_and, dpt.logical_or, dpt.logical_xor
+    def setup(self):
+        self.q = dpctl.SyclQueue(property='enable_profiling')
+        self.iterations = 1
+        self.zie = 2**27,
+        self.function_list = [
+            dpt.add,
+            dpt.multiply,
+            dpt.divide,
+            dpt.subtract,
+            dpt.floor_divide,
+            dpt.remainder,
+            dpt.hypot,
+            dpt.logaddexp,
+            dpt.pow,
+            dpt.atan2,
+            dpt.nextafter,
+            dpt.copysign,
+            dpt.less,
+            dpt.less_equal,
+            dpt.greater,
+            dpt.greater_equal,
+            dpt.equal,
+            dpt.not_equal,
+            dpt.minimum,
+            dpt.maximum,
+            dpt.bitwise_and,
+            dpt.bitwise_or,
+            dpt.bitwise_xor,
+            dpt.bitwise_left_shift,
+            dpt.bitwise_right_shift,
+            dpt.logical_and,
+            dpt.logical_or,
+            dpt.logical_xor
         ]
 
-    )
+        self.dtypes = {}
+        for fn in self.function_list:
+            self.dtypes[fn] = [list(map(dpt.dtype, sig.split('->')[0])) for sig in fn.types]
+
+        # Dynamically generate benchmark functions
+        self.generate_benchmark_functions()
 
 
-    def time_bench(self, q, reps, n_max, dtype, op):
-        self.run_bench2(q, reps, n_max, dtype, dtype, op)
+    def generate_benchmark_functions(self):
+        """Dynamically create benchmark functions for each function and dtype combination."""
+        for fn in self.function_list:
+            fn_name = fn.__name__
+            for dtype1, dtype2 in self.dtypes[fn]:
+                # Create a unique function name
+                method_name = f"time_{fn_name}_{dtype1.name}_{dtype2.name}"
+
+                # Define the benchmark function
+                def benchmark_method(self, fn=fn, dtype1=dtype1, dtype2=dtype2):
+                    return self.run_bench2(self.q, self.iterations, self.n_values, dtype1, dtype2, fn)
+
+                # Set the method name and attach it to the class
+                benchmark_method.__name__ = method_name
+                setattr(self.__class__, method_name, benchmark_method)
 
 
     def run_bench2(self, q, reps, n_max, dtype1, dtype2, op):
