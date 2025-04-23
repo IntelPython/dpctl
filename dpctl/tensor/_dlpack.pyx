949ac55776b1f553d2e90bc4bdcbea03a5386614
Nikita Grigorian <nikita.grigorian@intel.com>
2025-04-22 23:40:05 -0700

    Declare each Cython module free-threading compatible

--- dpctl/tensor/_dlpack.pyx
+++ dpctl/tensor/_dlpack.pyx
@@ -17,6 +17,7 @@
 # distutils: language = c++
 # cython: language_level=3
 # cython: linetrace=True
+# cython: freethreading_compatible = True
 
 cdef extern from "numpy/npy_no_deprecated_api.h":
     pass
