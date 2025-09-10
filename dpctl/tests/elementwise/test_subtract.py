830ccf0c8960956265fa3de17b612064a11a20b7
Nikita Grigorian <nikita.grigorian@intel.com>
2025-09-10 13:13:17 -0700

    remove redundant subtract tests

--- dpctl/tests/elementwise/test_subtract.py
+++ dpctl/tests/elementwise/test_subtract.py
@@ -14,17 +14,14 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-import ctypes
-
 import numpy as np
 import pytest
 
-import dpctl
 import dpctl.tensor as dpt
 from dpctl.tensor._type_utils import _can_cast
 from dpctl.tests.helper import get_queue_or_skip, skip_if_dtype_not_supported
 
-from .utils import _all_dtypes, _compare_dtypes, _usm_types
+from .utils import _all_dtypes, _compare_dtypes
 
 
 @pytest.mark.parametrize("op1_dtype", _all_dtypes[1:])
@@ -77,123 +74,6 @@ def test_subtract_bool():
         dpt.subtract(ar1, ar2)
 
 
-@pytest.mark.parametrize("op1_usm_type", _usm_types)
-@pytest.mark.parametrize("op2_usm_type", _usm_types)
-def test_subtract_usm_type_matrix(op1_usm_type, op2_usm_type):
-    get_queue_or_skip()
-
-    sz = 128
-    ar1 = dpt.ones(sz, dtype="i4", usm_type=op1_usm_type)
-    ar2 = dpt.ones_like(ar1, dtype="i4", usm_type=op2_usm_type)
-
-    r = dpt.subtract(ar1, ar2)
-    assert isinstance(r, dpt.usm_ndarray)
-    expected_usm_type = dpctl.utils.get_coerced_usm_type(
-        (op1_usm_type, op2_usm_type)
-    )
-    assert r.usm_type == expected_usm_type
-
-
-def test_subtract_order():
-    get_queue_or_skip()
-
-    test_shape = (
-        20,
-        20,
-    )
-    test_shape2 = tuple(2 * dim for dim in test_shape)
-    n = test_shape[-1]
-
-    for dt1, dt2 in zip(["i4", "i4", "f4"], ["i4", "f4", "i4"]):
-        ar1 = dpt.ones(test_shape, dtype=dt1, order="C")
-        ar2 = dpt.ones(test_shape, dtype=dt2, order="C")
-        r1 = dpt.subtract(ar1, ar2, order="C")
-        assert r1.flags.c_contiguous
-        r2 = dpt.subtract(ar1, ar2, order="F")
-        assert r2.flags.f_contiguous
-        r3 = dpt.subtract(ar1, ar2, order="A")
-        assert r3.flags.c_contiguous
-        r4 = dpt.subtract(ar1, ar2, order="K")
-        assert r4.flags.c_contiguous
-
-        ar1 = dpt.ones(test_shape, dtype=dt1, order="F")
-        ar2 = dpt.ones(test_shape, dtype=dt2, order="F")
-        r1 = dpt.subtract(ar1, ar2, order="C")
-        assert r1.flags.c_contiguous
-        r2 = dpt.subtract(ar1, ar2, order="F")
-        assert r2.flags.f_contiguous
-        r3 = dpt.subtract(ar1, ar2, order="A")
-        assert r3.flags.f_contiguous
-        r4 = dpt.subtract(ar1, ar2, order="K")
-        assert r4.flags.f_contiguous
-
-        ar1 = dpt.ones(test_shape2, dtype=dt1, order="C")[:20, ::-2]
-        ar2 = dpt.ones(test_shape2, dtype=dt2, order="C")[:20, ::-2]
-        r4 = dpt.subtract(ar1, ar2, order="K")
-        assert r4.strides == (n, -1)
-        r5 = dpt.subtract(ar1, ar2, order="C")
-        assert r5.strides == (n, 1)
-
-        ar1 = dpt.ones(test_shape2, dtype=dt1, order="C")[:20, ::-2].mT
-        ar2 = dpt.ones(test_shape2, dtype=dt2, order="C")[:20, ::-2].mT
-        r4 = dpt.subtract(ar1, ar2, order="K")
-        assert r4.strides == (-1, n)
-        r5 = dpt.subtract(ar1, ar2, order="C")
-        assert r5.strides == (n, 1)
-
-
-def test_subtract_broadcasting():
-    get_queue_or_skip()
-
-    m = dpt.ones((100, 5), dtype="i4")
-    v = dpt.arange(5, dtype="i4")
-
-    r = dpt.subtract(m, v)
-    assert (
-        dpt.asnumpy(r) == np.arange(1, -4, step=-1, dtype="i4")[np.newaxis, :]
-    ).all()
-
-    r2 = dpt.subtract(v, m)
-    assert (
-        dpt.asnumpy(r2) == np.arange(-1, 4, dtype="i4")[np.newaxis, :]
-    ).all()
-
-
-@pytest.mark.parametrize("arr_dt", _all_dtypes[1:])
-def test_subtract_python_scalar(arr_dt):
-    q = get_queue_or_skip()
-    skip_if_dtype_not_supported(arr_dt, q)
-
-    X = dpt.zeros((10, 10), dtype=arr_dt, sycl_queue=q)
-    py_zeros = (
-        bool(0),
-        int(0),
-        float(0),
-        complex(0),
-        np.float32(0),
-        ctypes.c_int(0),
-    )
-    for sc in py_zeros:
-        R = dpt.subtract(X, sc)
-        assert isinstance(R, dpt.usm_ndarray)
-        R = dpt.subtract(sc, X)
-        assert isinstance(R, dpt.usm_ndarray)
-
-
-@pytest.mark.parametrize("dtype", _all_dtypes[1:])
-def test_subtract_inplace_python_scalar(dtype):
-    q = get_queue_or_skip()
-    skip_if_dtype_not_supported(dtype, q)
-    X = dpt.zeros((10, 10), dtype=dtype, sycl_queue=q)
-    dt_kind = X.dtype.kind
-    if dt_kind in "ui":
-        X -= int(0)
-    elif dt_kind == "f":
-        X -= float(0)
-    elif dt_kind == "c":
-        X -= complex(0)
-
-
 @pytest.mark.parametrize("op1_dtype", _all_dtypes[1:])
 @pytest.mark.parametrize("op2_dtype", _all_dtypes[1:])
 def test_subtract_inplace_dtype_matrix(op1_dtype, op2_dtype):
@@ -221,15 +101,3 @@ def test_subtract_inplace_dtype_matrix(op1_dtype, op2_dtype):
     else:
         with pytest.raises(ValueError):
             ar1 -= ar2
-
-
-def test_subtract_inplace_broadcasting():
-    get_queue_or_skip()
-
-    m = dpt.ones((100, 5), dtype="i4")
-    v = dpt.arange(5, dtype="i4")
-
-    m -= v
-    assert (
-        dpt.asnumpy(m) == np.arange(1, -4, step=-1, dtype="i4")[np.newaxis, :]
-    ).all()
