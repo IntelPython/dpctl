80bf8aa20a169c63497d74c2fd2673ff331fe386
Nikita Grigorian <nikita.grigorian@intel.com>
2025-09-10 13:01:55 -0700

    remove redundant tests for positive and negative

--- dpctl/tests/elementwise/test_negative.py
+++ dpctl/tests/elementwise/test_negative.py
@@ -14,15 +14,13 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-import itertools
-
 import numpy as np
 import pytest
 
 import dpctl.tensor as dpt
 from dpctl.tests.helper import get_queue_or_skip, skip_if_dtype_not_supported
 
-from .utils import _all_dtypes, _usm_types
+from .utils import _all_dtypes
 
 
 @pytest.mark.parametrize("dtype", _all_dtypes[1:])
@@ -44,43 +42,3 @@ def test_negative_bool():
     x = dpt.ones(64, dtype="?")
     with pytest.raises(ValueError):
         dpt.negative(x)
-
-
-@pytest.mark.parametrize("usm_type", _usm_types)
-def test_negative_usm_type(usm_type):
-    q = get_queue_or_skip()
-
-    arg_dt = np.dtype("i4")
-    input_shape = (10, 10, 10, 10)
-    X = dpt.empty(input_shape, dtype=arg_dt, usm_type=usm_type, sycl_queue=q)
-    X[..., 0::2] = 1
-    X[..., 1::2] = 0
-
-    Y = dpt.negative(X)
-    assert Y.usm_type == X.usm_type
-    assert Y.sycl_queue == X.sycl_queue
-    assert Y.flags.c_contiguous
-
-    expected_Y = np.negative(dpt.asnumpy(X))
-    assert np.allclose(dpt.asnumpy(Y), expected_Y)
-
-
-@pytest.mark.parametrize("dtype", _all_dtypes[1:])
-def test_negative_order(dtype):
-    q = get_queue_or_skip()
-    skip_if_dtype_not_supported(dtype, q)
-
-    arg_dt = np.dtype(dtype)
-    input_shape = (10, 10, 10, 10)
-    X = dpt.empty(input_shape, dtype=arg_dt, sycl_queue=q)
-    X[..., 0::2] = 1
-    X[..., 1::2] = 0
-
-    for perms in itertools.permutations(range(4)):
-        U = dpt.permute_dims(X[:, ::-1, ::-1, :], perms)
-        expected_Y = np.negative(np.ones(U.shape, dtype=U.dtype))
-        expected_Y[..., 1::2] = 0
-        expected_Y = np.transpose(expected_Y, perms)
-        for ord in ["C", "F", "A", "K"]:
-            Y = dpt.negative(U, order=ord)
-            assert np.allclose(dpt.asnumpy(Y), expected_Y)
