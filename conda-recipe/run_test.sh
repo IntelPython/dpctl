d4085e4ac1ae0fbf78de9a50b93e8e5a028143c1
Harlow, Jordan <jordan.harlow@intel.com>
2025-09-12 10:12:33 -0600

    chore: no run_test default scripts

--- conda-recipe/run_test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-
-${PYTHON} -c "import dpctl; print(dpctl.__version__)"
-${PYTHON} -m dpctl -f
-${PYTHON} -m pytest -q -ra --disable-warnings --cov dpctl --cov-report term-missing --pyargs dpctl -vv
