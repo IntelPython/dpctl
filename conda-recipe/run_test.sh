4ecd31dda1a9bdcd3d8a58f531b9156414f0001b
Nikita Grigorian <nikita.grigorian@intel.com>
2026-02-17 04:06:01 -0800

    remove pytest-cov as test dependencies

--- conda-recipe/run_test.sh
+++ conda-recipe/run_test.sh
@@ -4,11 +4,4 @@ set -e
 
 ${PYTHON} -c "import dpctl; print(dpctl.__version__)"
 ${PYTHON} -m dpctl -f
-# don't use coverage for Python 3.13 due to crashes related to
-# Cython >= 3.1.0 and Python >= 3.13
-# TODO: remove if crash is triaged
-if ${PYTHON} --version 2>&1 | grep -q '^Python 3\.13'; then
-    ${PYTHON} -m pytest -q -ra --disable-warnings --pyargs dpctl -vv
-else
-    ${PYTHON} -m pytest -q -ra --disable-warnings --cov dpctl --cov-report term-missing --pyargs dpctl -vv
-fi
+${PYTHON} -m pytest -q -ra --disable-warnings --pyargs dpctl -vv
